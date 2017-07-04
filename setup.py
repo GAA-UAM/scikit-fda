417766bc4efb27aec7c55466b0bc55b467d687b6
mcarbajo <emecarbajo@gmail.com>
2017-07-04 11:33:15 +0200

    commit inicial

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='fda',
+      version='0.1',
+      description='Functional Data Analysis Python package',
+      url='https://github.com/mcarbajo/fda',
+      author='Flying Circus',
+      author_email='miguel.carbajo@estudiante.uam.es',
+      license='???',
+      packages=['fda'],
+      install_requires=[
+          'numpy',
+      ],
+      zip_safe=False)

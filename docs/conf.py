04d68ab3722f62b40e48c1c2be8b3d7f66760302
mcarbajo <emecarbajo@gmail.com>
2018-02-13 14:52:38 +0100

    Reverting last changes

--- docs/conf.py
+++ docs/conf.py
@@ -51,8 +51,6 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.napoleon',
               'sphinx.ext.mathjax',]
 
-autodoc_mock_imports = ['_tkinter']
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 

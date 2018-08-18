8f4a8ac1542c113cab19c7446a923482f892954b
pablomm <pablo.marcosm@estudiante.uam.es>
2018-08-18 02:28:52 +0200

    Finished shift registration (except tests). Added rescale method on basis

--- fda/basis.py
+++ fda/basis.py
@@ -252,6 +252,24 @@ class Basis(ABC):
         """
         pass
 
+    def rescale(self, domain_range=None):
+        r"""Return a copy of the basis with a new domain range, with the
+            corresponding values rescaled to the new bounds.
+
+            Args:
+                domain_range (tuple, optional): Definition of the interval where
+                    the basis defines a space. Defaults uses the same as the
+                    original basis.
+        """
+
+        if domain_range == None:
+            domain_range = self.domain_range
+        elif domain_range[0] >= domain_range[1]:
+            raise ValueError("The interval {} is not well-defined.".format(
+                domain_range))
+
+        return type(self)(domain_range, self.nbasis)
+
     def __repr__(self):
         """Representation of a Basis object."""
         return "{}(domain_range={}, nbasis={})".format(
@@ -754,6 +772,34 @@ class BSpline(Basis):
         # compute using the inner product
         return self._numerical_penalty(coefficients)
 
+    def rescale(self, domain_range=None):
+        r"""Return a copy of the basis with a new domain range, with the
+            corresponding values rescaled to the new bounds.
+            The knots of the BSpline will be rescaled in the new interval.
+
+            Args:
+                domain_range (tuple, optional): Definition of the interval where
+                    the basis defines a space. Defaults uses the same as the
+                    original basis.
+        """
+
+        knots = numpy.array(self.knots, dtype=numpy.dtype('float'))
+
+        if domain_range != None:  # Rescales the knots
+            knots -= knots[0]
+            knots *= ((domain_range[1] - domain_range[0]
+                       ) / (self.knots[-1] - self.knots[0]))
+            knots += domain_range[0]
+        else:
+            domain_range = self.domain_range
+
+        # Fix possible round error
+        knots[0] = domain_range[0]
+        knots[-1] = domain_range[1]
+
+        return BSpline(domain_range, self.nbasis, self.order, knots)
+
+
     def __repr__(self):
         """Representation of a BSpline basis."""
         return ("{}(domain_range={}, nbasis={}, order={}, knots={})".format(
@@ -911,7 +957,7 @@ class Fourier(Basis):
         The differential operator can be either a derivative of a certain
         degree or a more complex operator.
 
-        The penalty matrix is defined as [RS05-5-6-2]_:
+        The penalty matrix is defined as [RS05-5-6-2]:
 
         .. math::
             R_{ij} = \int L\phi_i(s) L\phi_j(s) ds
@@ -962,6 +1008,30 @@ class Fourier(Basis):
         else:
             # implement using inner product
             return self._numerical_penalty(coefficients)
+
+    def rescale(self, domain_range=None, rescale_period=False):
+        r"""Return a copy of the basis with a new domain range, with the
+            corresponding values rescaled to the new bounds.
+
+            Args:
+                domain_range (tuple, optional): Definition of the interval where
+                    the basis defines a space. Defaults uses the same as the
+                    original basis.
+                rescale_period (bool, optional): If true the period will be
+                    rescaled using the ratio between the lengths of the new
+                    and old interval. Defaults to False.
+        """
+
+        rescale_basis = super().rescale(domain_range)
+
+        if rescale_period == False:
+            rescale_basis.period = self.period
+        else:
+            rescale_basis.period = self.period * \
+                (rescale_basis.domain_range[1] - rescale_basis.domain_range[0]
+                 ) / (self.domain_range[1] - self.domain_range[0])
+
+        return rescale_basis
 
     def __repr__(self):
         """Representation of a Fourier basis."""
@@ -1270,7 +1340,8 @@ class FDataBasis:
 
         return res_matrix
 
-    def _evaluate_shifted(self, eval_points, delta, derivative=0):
+    def _evaluate_shifted(self, eval_points, delta, derivative=0,
+                          periodic_extrapolation=False):
         """Evaluate the object or its derivatives at a list of values.
 
         This method has to evaluate the basis values once per sample and
@@ -1282,10 +1353,18 @@ class FDataBasis:
                 evaluated.
             delta (array_like): List of shifts for each function
             derivative (int, optional): Order of the derivative. Defaults to 0.
-
+            periodic_extrapolation (bool, optional): If true extrapolates the
+                times assuming that the functions are periodic in the
+                domain_range. Useful for periodic data modeled with non
+                periodic basis. Else the evaluation of points outside the domain
+                is not defined.
         Returns:
             (numpy.darray): Matrix whose rows are the values of the each
             function at the values specified in eval_points.
+
+        Todo:
+            See how to initialize the _matrix variable and use it as outpot
+            of the matrix product.
 
         """
 
@@ -1299,80 +1378,132 @@ class FDataBasis:
                              .format(delta.shape[0], self.nsamples))
 
         res_matrix = numpy.empty((self.nsamples, eval_points.shape[0]))
+        shifted_points = numpy.empty(len(eval_points))
+        domain_length = self.domain_range[1] - self.domain_range[0]
 
         for i in range(self.nsamples):
 
             # each column contains the values of one element of the basis
             # evaluated in the shifted time delta[i]
-            basis_values = self.basis.evaluate(eval_points + delta[i],
-                                               derivative).T
+            numpy.add(eval_points, delta[i], shifted_points)
+
+            if periodic_extrapolation:
+                numpy.subtract(
+                    shifted_points, self.domain_range[0], shifted_points)
+                numpy.mod(shifted_points, domain_length, shifted_points)
+                numpy.add(shifted_points, self.domain_range[0], shifted_points)
+
+            basis_values = self.basis.evaluate(shifted_points, derivative).T
 
             _matrix = basis_values * self.coefficients[i]
             res_matrix[i] = _matrix.sum(axis=1)
 
         return res_matrix
 
-    def shift_registration(self, maxiter=3, tol=1e-2, alpha=1, initial=None,
-                           maxshift=None, periodic=None, nfine=None):
-        r"""Perform a shift registration of the curves
+    def shift_registration(self, maxiter=3, tol=1e-2, periodic="default",
+                           alpha=1, initial=None, tfine=None,
+                           shifts_array=False, **kwargs):
+        r"""Perform a shift registration of the curves.
 
-        TODO: See convergence criterions.
-              Non-periodic basis and data
-              Description and examples
-              Tests
-              See method to convert data without qr
-              Add periodic attribute to basis?
+            Realizes a registration of the curves, using shift aligment, as is
+            defined in [RS05-7-2]_. Calculates :math:`\delta_{i}` such that
+            :math:`x_i(t + \delta_{i})` minimizes the least squares criterion:
 
-        Description:
-            TODO
+            .. math::
+                \text{REGSSE} = \sum_{i=1}^{N} \int_{\mathcal{T}}
+                [x_i(t + \delta_i) - \hat\mu(t)]^2 ds
+
+            Estimates the shift parameter :math:`\delta_i` iteratively by
+            using a modified Newton-Raphson algorithm, updating the mean
+            in each iteration, as is described in detail in [RS05-7-9-1]_.
 
         Args:
-            maxiter (int, optional):
-            tol (float, optional):
-            alpha (int or float, optional):
-            initial (array_like, optional):
-            maxshift (float, optional):
-            periodic (bool, optional):
-            nfine (int, optional):
+            maxiter (int, optional): Maximun number of iterations.
+                Default sets to 3.
+            tol (float, optional): Tolerance allowable. The process will stop if
+                :math:`\max_{i}|\delta_{i}^{(\nu)}-\delta_{i}^{(\nu-1)}|<tol`.
+                Default sets to 1e-2.
+            periodic (bool, optional): If is True the functions are considered
+                periodical, if the basis of the functions is periodic it is used
+                its own period, as in the case of :meth:`Fourier` basis, else
+                are considerated periodic in the domain range, as in the
+                periodic :meth:`BSplines` case. In general, it is not
+                recommended this option for non periodic basis.
+                If it is False the domain range of the functions are restricted
+                to allow evaluated all the functions with their shifts.
+                If 'default' are passed it is choosen according to the basis,
+                being True for periodic basis and False in other cases.
+            alpha (int or float, optional): Parameter to adjust the rate of
+                convergence in the Newton-Raphson algorithm, see [RS05-7-9-1]_.
+                Defaults to 1.
+            initial (array_like, optional): Initial estimation of shifts.
+                Default uses a list of zeros for the initial shifts.
+            tfine (array_like, optional): Set of points where the
+                functions are evaluated to obtain the discrete
+                representation of the object to integrate. If none are passed
+                it calls numpy.linspace with bounds equal to the ones defined in
+                self.domain_range and the number of points the maximum
+                between 201 and 10 times the number of basis plus 1.
+            shifts_array (bool, optional): If True returns an array with the
+                shifts instead of a :obj:`FDataBasis` with the registered
+                curves. Default sets to False.
+            **kwargs: Keyword arguments to be passed to the :meth:`from_data`
+                function.
 
         Returns:
-            :obj:`FDataBasis`: A FDataBasis object with the samples registered.
+            :obj:`FDataBasis` or :obj:`ndarray`: A :obj:`FDataBasis` object with
+            the curves registered or if shifts_array is True a :obj:`ndarray`
+            with the shifts.
 
-        Examples:
-            TODO
+        Raises:
+            ValueError: If the initial array has different length than the
+                number of samples.
 
         References:
-            .. [RS05-7-2] Ramsay, J., Silverman, B. W. (2005). Shift
+            ..  [RS05-7-2] Ramsay, J., Silverman, B. W. (2005). Shift
                 registration. In *Functional Data Analysis* (pp. 129-132).
-                Springler.
-
-            .. [RS05-7-9-1] Ramsay, J., Silverman, B. W. (2005). Shift
-            registration by the Newton-Raphson algorithm. In *Functional Data
-            Analysis* (pp. 142-144). Springler.
-
+                Springer.
+            ..  [RS05-7-9-1] Ramsay, J., Silverman, B. W. (2005). Shift
+                registration by the Newton-Raphson algorithm. In *Functional
+                Data Analysis* (pp. 142-144). Springer.
         """
 
-        if periodic == False:
-            raise NotImplemented("Shift aligment for not periodic data not "
-                                 "implemented yet")
-
+        # Initial estimation of the shifts
         if initial == None:
-            initial = numpy.zeros(self.nsamples)
+            delta = numpy.zeros(self.nsamples)
 
         elif len(initial) != self.nsamples:
-            raise ValueError("the initial shift ({}) must have the same length"
+            raise ValueError("the initial shift ({}) must have the same length "
                              "than the number of samples ({})"
                              .format(len(initial), self.nsamples))
-
-        if not nfine:
-            nfine = max(self.nbasis*10+1, 201)  # Used in R version
+        else:
+            delta = numpy.asarray(initial)
 
         # Fine equispaced mesh to evaluate the samples
-        tfine = numpy.linspace(self.basis.domain_range[0],
-                               self.basis.domain_range[1],
-                               nfine)
+        if tfine == None:
+            nfine = max(self.nbasis*10+1, 201)  # Used in R version
+            tfine = numpy.linspace(self.basis.domain_range[0],
+                                   self.basis.domain_range[1],
+                                   nfine)
+        else:
+            nfine = len(tfine)
+            tfine = numpy.asarray(tfine)
 
-        # Compute the derivate of originals curves in the mesh points
+        if periodic == "default":
+            # Periodic BSplines could be a good idea
+            periodic = True if isinstance(self.basis, Fourier) else False
+
+        # The periodic extrapolation is not needed in Fourier basis
+        if not periodic or isinstance(self.basis, Fourier):
+            periodic_ext = False
+        else:
+            periodic_ext = True
+
+        # Auxiliar arrays to avoid multiple memory allocations
+        delta_aux = numpy.empty(self.nsamples)
+        tfine_aux = numpy.empty(nfine)
+
+        # Computes the derivate of originals curves in the mesh points
         D1x = self.evaluate(tfine, 1)
 
         # Second term of the second derivate estimation of REGSSE. The
@@ -1380,46 +1511,70 @@ class FDataBasis:
         d2_regsse = scipy.integrate.trapz(numpy.square(D1x), tfine, axis=1)
 
         max_diff = tol + 1
-        i = 0
-        delta = numpy.asarray(initial)
-        last_delta = numpy.zeros(self.nsamples)
+        iter = 0
+        a = 0
+        b = 0
 
-        while max_diff > tol and i < maxiter:
+        # Saves the array in all the interval if it will be adjusted to slice
+        if not periodic:
+            D1x_tmp = D1x
+            tfine_tmp = tfine
+            tfine_aux_tmp = tfine_aux
+            domain = numpy.empty(nfine, dtype=numpy.dtype(bool))
 
-            x = self._evaluate_shifted(tfine, delta)
-            mean = numpy.mean(x, axis=0)
+        # Newton-Rhapson iteration
+        while max_diff > tol and iter < maxiter:
 
-            # Calculate x - mean without allocate other ndarray
-            numpy.subtract(x, mean, x)
+            # Updates the limits for non periodic functions ignoring the ends
+            if not periodic:
+                # Calculates the new limits
+                a = min(numpy.min(delta), 0)
+                b = max(numpy.max(delta), 0)
 
-            # Real REGSSE derivates are multiplied by 2, but the division
-            # simplifies it
-            d1_regsse = scipy.integrate.trapz(numpy.multiply(x, D1x),
+                # The new interval is (domain[0] - a, domain[1] - b)
+                numpy.logical_and(tfine_tmp >= self.basis.domain_range[0] - a,
+                                  tfine_tmp <= self.basis.domain_range[1] - b,
+                                  out=domain)
+                tfine = tfine_tmp[domain]
+                tfine_aux = tfine_aux_tmp[domain]
+                D1x = D1x_tmp[:, domain]
+                # Reescale the second derivate could be other approach
+                # d2_regsse =
+                # d2_regsse_original * ( 1 + (a - b) / (domain[1] - domain[0]))
+                d2_regsse = scipy.integrate.trapz(numpy.square(D1x), tfine,
+                                                  axis=1)
+
+            # Computes the new values shifted
+            x = self._evaluate_shifted(tfine, delta,
+                                       periodic_extrapolation=periodic_ext)
+            x.mean(axis=0, out=tfine_aux)
+
+            # Calculates x - mean
+            numpy.subtract(x, tfine_aux, out=x)
+
+            # REGSSE derivates are multiplied by 2
+            d1_regsse = scipy.integrate.trapz(numpy.multiply(x, D1x, out=x),
                                               tfine, axis=1)
+            # Updates the shifts by the Newton-Rhapson iteration
+            # delta = delta - alpha * d1_regsse / d2_regsse
+            numpy.divide(d1_regsse, d2_regsse, out=delta_aux)
+            numpy.multiply(delta_aux, alpha, out=delta_aux)
+            numpy.subtract(delta, delta_aux, out=delta)
 
-            # Equivalent to delta = delta - alpha * d1_regsse / d2_regsse
-            # Without allocate others ndarrays
-            numpy.divide(d1_regsse, d2_regsse, d1_regsse)
-            numpy.multiply(d1_regsse, alpha, d1_regsse)
-            numpy.subtract(delta, d1_regsse, delta)
+            # Updates convergence criterions
+            max_diff = numpy.abs(delta_aux, out=delta_aux).max()
+            iter += 1
 
-            if maxshift:
-                numpy.minimum(delta, maxshift, delta)
-                numpy.maximum(delta, -1 * maxshift, delta)
+        # If shifts_array is True returns the delta array
+        if shifts_array:
+            return delta
 
-            # Update convergence criterions
-            i += 1
-            max_diff = numpy.max(
-                numpy.abs(
-                    numpy.subtract(delta, last_delta, last_delta), last_delta))
-
-            numpy.copyto(last_delta, delta)
-
-
-        # Computes the curves with the final shift
+        # Computes the values with the final shift to construct the FDataBasis
         x = self._evaluate_shifted(tfine, delta)
+        rescaled_basis = self.basis.rescale(
+            (self.domain_range[0] - a, self.domain_range[1] - b))
 
-        return FDataBasis.from_data(x, tfine, self.basis, method='qr')
+        return FDataBasis.from_data(x, tfine, rescaled_basis, **kwargs)
 
     def plot(self, ax=None, derivative=0, **kwargs):
         """Plot the FDataBasis object or its derivatives.

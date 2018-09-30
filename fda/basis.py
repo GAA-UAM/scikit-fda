0a1a702b2642a6814de3ddb26e18764df01dbcf2
pablomm <pablo.marcosm@estudiante.uam.es>
2018-09-30 15:45:00 +0200

    Bug fixed in landmark_shift

--- fda/basis.py
+++ fda/basis.py
@@ -1545,7 +1545,7 @@ class FDataBasis:
 
 
     def landmark_shift(self, landmarks, location='minimize', ext='default',
-                       tfine=None, shifts_array=False, **kwargs):
+                       tfine=[], shifts_array=False, **kwargs):
         r"""
 
         """

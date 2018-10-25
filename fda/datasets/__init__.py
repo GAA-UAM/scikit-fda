447f6824664a85e7854beecca08d4e0aa75ce3f8
VNMabus <vnmabus@gmail.com>
2018-10-25 15:16:11 +0200

    Change load_* by fetch_*, as the datasets are downloaded, to follow Sklearn convention

--- fda/datasets/__init__.py
+++ fda/datasets/__init__.py
@@ -1,4 +1,4 @@
 from ._real_datasets import (fdata_constructor, fetch_cran_dataset,
-                             load_phoneme, load_growth,
-                             load_tecator)
+                             fetch_phoneme, fetch_growth,
+                             fetch_tecator)
 from ._samples_generators import make_gaussian_process

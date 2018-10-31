d090c0afd5f6e1ecc1369d781d6cbe217176ba5a
pablomm <pablo.marcosm@estudiante.uam.es>
2018-10-31 10:56:03 +0100

    Sinusoidal dataset

--- fda/datasets/__init__.py
+++ fda/datasets/__init__.py
@@ -1,4 +1,4 @@
 from ._real_datasets import (fdata_constructor, fetch_cran_dataset,
                              fetch_phoneme, fetch_growth,
                              fetch_tecator, fetch_medflies)
-from ._samples_generators import make_gaussian_process
+from ._samples_generators import make_gaussian_process, make_sinusoidal_process

73842ba34696abfb59d4ed4a07d685a0e6f9db0a
VNMabus <vnmabus@gmail.com>
2018-11-07 16:12:29 +0100

    Added medflies dataset
    Small dataset corrections

--- fda/datasets/__init__.py
+++ fda/datasets/__init__.py
@@ -1,4 +1,4 @@
 from ._real_datasets import (fdata_constructor, fetch_cran_dataset,
                              fetch_phoneme, fetch_growth,
-                             fetch_tecator)
+                             fetch_tecator, fetch_medflies_classification)
 from ._samples_generators import make_gaussian_process
--- fda/datasets/_real_datasets.py
+++ fda/datasets/_real_datasets.py
@@ -1,6 +1,5 @@
 import numpy as np
 import rdata
-import skdatasets
 
 from ..grid import FDataGrid
 
@@ -15,8 +14,38 @@ def fdata_constructor(obj, attrs):
                      axes_labels=[names['xlab'][0], names['ylab'][0]])
 
 
+def functional_constructor(obj, attrs):
+
+    name = obj['name']
+    args_label = obj['args']
+    values_label = obj['vals']
+    target = np.array(obj['labels']).ravel()
+    dataf = obj['dataf']
+
+    sample_points_set = {a for o in dataf for a in o["args"]}
+
+    args_init = min(sample_points_set)
+    args_end = max(sample_points_set)
+
+    sample_points = np.arange(args_init,
+                              args_end + 1)
+
+    data_matrix = np.zeros(shape=(len(dataf), len(sample_points)))
+
+    for num_sample, o in enumerate(dataf):
+        for t, x in zip(o["args"], o["vals"]):
+            data_matrix[num_sample, t - args_init] = x
+
+    return (FDataGrid(data_matrix=data_matrix,
+                      sample_points=sample_points,
+                      sample_range=(args_init, args_end),
+                      dataset_label=name[0],
+                      axes_labels=[args_label[0], values_label[0]]), target)
+
+
 def fetch_cran_dataset(dataset_name, package_name, *, converter=None,
                        **kwargs):
+    import skdatasets
     """
     Fetch a dataset from CRAN.
 
@@ -24,7 +53,8 @@ def fetch_cran_dataset(dataset_name, package_name, *, converter=None,
     if converter is None:
         converter = rdata.conversion.SimpleConverter({
             **rdata.conversion.DEFAULT_CLASS_MAP,
-            "fdata": fdata_constructor})
+            "fdata": fdata_constructor,
+            "functional": functional_constructor})
 
     return skdatasets.cran.fetch_dataset(dataset_name, package_name,
                                          converter=converter, **kwargs)
@@ -89,8 +119,8 @@ def fetch_phoneme(return_X_y: bool=False):
     DESCR = _phoneme_descr
 
     raw_dataset = fetch_cran_dataset(
-        "phoneme.RData", "ElemStatLearn",
-        package_url="https://cran.r-project.org/src/contrib/Archive/ElemStatLearn/ElemStatLearn_0.1-7.1.tar.gz")
+        "phoneme", "ElemStatLearn",
+        version="0.1-7.1")
 
     data = raw_dataset["phoneme"]
 
@@ -142,8 +172,8 @@ def fetch_growth(return_X_y: bool=False):
     DESCR = _growth_descr
 
     raw_dataset = fetch_cran_dataset(
-        "growth.rda", "fda",
-        package_url="https://cran.r-project.org/src/contrib/Archive/fda/fda_2.4.7.tar.gz")
+        "growth", "fda",
+        version="2.4.7")
 
     data = raw_dataset["growth"]
 
@@ -220,8 +250,8 @@ def fetch_tecator(return_X_y: bool=False):
     DESCR = _tecator_descr
 
     raw_dataset = fetch_cran_dataset(
-        "tecator.rda", "fda.usc",
-        package_url="https://cran.r-project.org/src/contrib/Archive/fda.usc/fda.usc_1.3.0.tar.gz")
+        "tecator", "fda.usc",
+        version="1.3.0")
 
     data = raw_dataset["tecator"]
 
@@ -240,3 +270,74 @@ def fetch_tecator(return_X_y: bool=False):
 
 if hasattr(fetch_tecator, "__doc__"):  # docstrings can be stripped off
     fetch_tecator.__doc__ += _tecator_descr + _param_descr
+
+_medflies_descr = """
+    The data set medfly1000.dat  consists of number of eggs laid daily for
+    each of 1000 medflies (Mediterranean fruit flies, Ceratitis capitata)
+    until time of death. Data were obtained in Dr. Carey's laboratory.
+    A description of the experiment which was done by Professor Carey of
+    UC Davis and collaborators in a medfly rearing facility in
+    Mexico is in Carey et al.(1998) below. The main questions are to
+    explore the relationship of age patterns of fecundity to mortality,
+    longevity and lifetime reproduction.
+
+    A basic finding was that individual mortality is associated with the
+    time-dynamics of the egg-laying trajectory. An approximate parametric model
+    of the egg laying process was developed and used in Müller et al. (2001).
+    Nonparametric approaches which extend principal component analysis for
+    curve data to the situation when covariates are present have been
+    developed and discussed in  Chiou, Müller and Wang (2003)
+    and Chiou et al. (2003).
+
+    Carey, J.R., Liedo, P., Müller, H.G., Wang, J.L., Chiou, J.M. (1998).
+        Relationship of age patterns of fecundity to mortality, longevity,
+        and lifetime reproduction in a large cohort of Mediterranean fruit
+        fly females. J. of Gerontology --Biological Sciences 53, 245-251.
+    Chiou, J.M., Müller, H.G., Wang, J.L. (2003). Functional quasi-likelihood
+        regression models with smooth random effects. J. Royal Statist. Soc.
+        B65, 405-423. (PDF)
+    Chiou, J.M., Müller, H.G., Wang, J.L., Carey, J.R. (2003). A functional
+        multiplicative effects model for longitudinal data, with application to
+        reproductive histories of female medflies. Statistica Sinica 13,
+        1119-1133. (PDF)
+    Chiou, J.M., Müller, H.G., Wang, J.L. (2004).Functional response models.
+        Statistica Sinica 14,675-693. (PDF)
+"""
+
+
+def fetch_medflies_classification(return_X_y: bool=False):
+    """
+    Load the Medflies dataset, where the flies are separated in two classes
+    according to their longevity.
+
+    The data is obtained from the R package 'ddalpha', which its a modification
+    of the dataset in http://www.stat.ucdavis.edu/~wang/data/medfly1000.htm.
+
+    """
+    DESCR = _medflies_descr
+
+    raw_dataset = fetch_cran_dataset(
+        "medflies", "ddalpha",
+        version="1.3.4")
+
+    data = raw_dataset["medflies"]
+
+    curves = data[0]
+
+    unique = np.unique(data[1], return_inverse=True)
+    target_names = [unique[0][1], unique[0][0]]
+    target = 1 - unique[1]
+    target_feature_names = ["lifetime"]
+
+    if return_X_y:
+        return curves, target
+    else:
+        return {"data": curves,
+                "target": target,
+                "target_names": target_names,
+                "target_feature_names": target_feature_names,
+                "DESCR": DESCR}
+
+
+if hasattr(fetch_medflies_classification, "__doc__"):  # docstrings can be stripped off
+    fetch_medflies_classification.__doc__ += _medflies_descr + _param_descr

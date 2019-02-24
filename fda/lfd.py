603033617d327c5b905fddf2ea94037176c35394
Pablo Manso <92manso@gmail.com>
2019-02-24 09:14:40 +0100

    Fixed test assertes

--- fda/lfd.py
+++ fda/lfd.py
@@ -10,67 +10,87 @@ class Lfd:
     """Defines the structure of a linear differential operator function system
 
     .. math::
-        Lx(t) = b_0(t) x(t) + b_1(t) x'(x) + \dots + b_{n-1}(t) d^{n-1}(x(t)) + b_n(t) d^n(x(t))
+        Lx(t) = b_0(t) x(t) + b_1(t) x'(x) +
+                \dots + b_{n-1}(t) d^{n-1}(x(t)) + b_n(t) d^n(x(t))
 
     Attributes:
-        nderiv (int): the order of the operator. It's the n coefficient in the equation above.
+        order (int): the order of the operator. It's the n coefficient in the
+                     equation above.
 
-        bwtlist (list):  A FDataBasis objects list of length either nderiv
+        weights (list):  A FDataBasis objects list of length either nderiv
 
     """
 
-    def __init__(self, param=0, domain_range=(0, 1)):
-        """Lfd Constructor
+    def __init__(self, order=None, weights=None, domain_range=(0, 1)):
+        """Lfd Constructor. You have to provide one of the two first
+           parameters. It both are provided, it will raise an error
 
         Args:
-            param (int, list, FDataBasis optional): If the given parameter is an integer, it'll be the order of
-                the highest derivative.
-                  If its a list, of integers or FDataBasis objects, it will be taken as the weight functions
-                  for the derivatives.
-                  If its a FDataBasis object, each sample will be a weight function and nderiv will be the number
-                  of samples
+            order (int, optional): the order of the operator. It's the n
+                  coefficient in the equation above.
 
-            domain_range (tuple or list of tuples, optional): Definition of the
-                interval where the weight functions are defined. Defaults to (0,1).
+            weights (list, optional): A FDataBasis objects list of length
+                     either nderiv
+
+            domain_range (tuple or list of tuples, optional): Definition
+                         of the interval where the weight functions are
+                         defined. Defaults to (0,1).
         """
 
-        if isinstance(param, list):
-            if all(isinstance(n, int) for n in param):
-                self.nderiv = len(param)
-                self.bwtlist = (FDataBasis(Constant(domain_range), np.array(param).reshape(-1, 1)).to_list()
-                                if self.nderiv != 0 else [])
-            elif all(isinstance(n, FDataBasis) for n in param):
-                if all([_same_domain(param[0].domain_range, x.domain_range) for x in param]):
-                    self.nderiv = len(param)
-                    self.bwtlist = param
-                else:
-                    raise ValueError("FDataBasis objects in the list has not the same domain_range")
-            else:
-                raise ValueError("The elements of the list are neither integers or FDataBasis objects")
+        if order is not None and weights is not None:
+            raise ValueError("You have to provide the order or the weights, "
+                             "not both")
 
-        elif isinstance(param, FDataBasis):
-            self.nderiv = param.nsamples
-            self.bwtlist = param.to_list()
+        self.domain_range = domain_range
 
-        elif isinstance(param, int):
-            if param < 0:
-                raise ValueError("The deriv order must be non-negative.")
+        if order is None and weights is None:
+            self.order = 0
+            self.weights = []
 
-            self.nderiv = param
-            self.bwtlist = [FDataBasis(Constant(domain_range), 0) for _ in range(param)] if self.nderiv != 0 else []
+        elif weights is None:
+            if order < 0:
+                raise ValueError("Order should be an non-negative integer")
+
+            self.order = order
+            self.weights = [
+                FDataBasis(Constant(domain_range), 0 if (i < order) else 1) for
+                i in range(order + 1)]
 
         else:
-            raise ValueError("Argument is neither a non-negative integer or list")
+            if len(weights) is 0:
+                raise ValueError("You have to provide one weight at least")
+
+            if all(isinstance(n, int) for n in weights):
+                self.order = len(weights) - 1
+                self.weights = (FDataBasis(Constant(domain_range),
+                                           np.array(weights)
+                                           .reshape(-1, 1)).to_list())
+
+            elif all(isinstance(n, FDataBasis) for n in weights):
+                if all([_same_domain(weights[0].domain_range,
+                                     x.domain_range) and x.nsamples == 1 for x
+                        in weights]):
+                    self.order = len(weights)
+                    self.weights = weights
+                    self.domain_range = weights[0].domain_range
+
+                else:
+                    raise ValueError("FDataBasis objects in the list has "
+                                     "not the same domain_range")
+
+            else:
+                raise ValueError("The elements of the list are neither "
+                                 "integers or FDataBasis objects")
 
     def __repr__(self):
         """Representation of Lfd object."""
 
         bwtliststr = ""
-        for i in range(self.nderiv):
-            bwtliststr = bwtliststr + "\n" + self.bwtlist[i].__repr__() + ","
+        for i in range(self.order):
+            bwtliststr = bwtliststr + "\n" + self.weights[i].__repr__() + ","
 
         return (f"{self.__class__.__name__}("
-                f"\nnderiv={self.nderiv},"
+                f"\nnderiv={self.order},"
                 f"\nbwtlist=[{bwtliststr[:-1]}]"
                 f"\n)").replace('\n', '\n    ')
 
@@ -81,11 +101,6 @@ class Lfd:
 
     def __eq__(self, other):
         """Equality of Lfd objects"""
-        return (self.nderiv == other.nderic
-                and all(self.bwtlist[i] == other.bwtlist[i] for i in range(self.nderiv)))
-
-    @property
-    def domain_range(self):
-        """Definition range."""
-
-        return None if self.nderiv == 0 else self.bwtlist[0].domain_range
+        return (self.order == other.nderic
+                and all(self.weights[i] == other.bwtlist[i]
+                        for i in range(self.order)))

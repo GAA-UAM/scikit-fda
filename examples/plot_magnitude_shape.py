80050f194c9b26842bdcbc3434452149c0ffc82e
ahernando <amanda.hernando@estudiante.uam.es>
2019-02-25 17:48:20 +0100

    changes in doc

--- examples/plot_magnitude_shape.py
+++ examples/plot_magnitude_shape.py
@@ -12,12 +12,13 @@ Shows the use of the MS-Plot applied to the Canadian Weather dataset.
 
 from fda import datasets
 from fda.grid import FDataGrid
-from fda.depth_measures import Fraiman_Muniz_depth
+from fda.depth_measures import fraiman_muniz_depth
 from fda.magnitude_shape_plot import magnitude_shape_plot
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
 import matplotlib.patches as mpatches
+from cycler import cycler
+import matplotlib
 
 ##################################################################################
 # First, the Canadian Weather dataset is downloaded from the package 'fda' in CRAN.
@@ -29,35 +30,29 @@ dataset = datasets.fetch_weather()
 fd = dataset["data"]
 fd_temperatures = FDataGrid(data_matrix=fd.data_matrix[:, :, 0], sample_points=fd.sample_points,
                             dataset_label=fd.dataset_label, axes_labels=fd.axes_labels[0:2])
-
 ############################################################################################
 # The data is plotted to show the curves we are working with. They are divided according to the
 # target. In this case, it includes the different climates to which the weather stations belong to.
 
-regions = dataset["target"]
-indexer, uniques = pd.factorize(regions, sort=True)
+climates = dataset["target_names"]
+n_climates = len(climates)
+# indexer, uniques = pd.factorize(regions, sort=True)
 
 #Assigning the color to each of the samples.
 colormap = plt.cm.get_cmap('seismic')
-colors_climate = colormap(indexer / (len(uniques) - 1))
+colors_by_climate = colormap(np.asarray(dataset["target"]) / (n_climates- 1))
+climate_colors = colormap(np.arange(n_climates) / (n_climates- 1))
+
+#Building the legend
+patches = []
+for i in range(n_climates):
+    patches.append(mpatches.Patch(color=climate_colors[i], label=climates[i]))
 
 #Plotting the curves.
+matplotlib.rcParams['axes.prop_cycle'] = cycler(color=colors_by_climate)
 plt.figure()
-for i in range(fd_temperatures.nsamples):
-    plt.plot(fd_temperatures.sample_points[0], fd_temperatures.data_matrix[i], c=colors_climate[i])
-
-#Plotting the legend
-indices = range(len(uniques))
-d = dict(zip(uniques, indices))
-patches = []
-for label, index in d.items():
-    patches.append(mpatches.Patch(color=colormap(index / (len(uniques) - 1)), label=label))
-plt.legend(handles=patches)
-
-#Naming axes
-plt.xlabel(fd_temperatures.axes_labels[0])
-plt.ylabel(fd_temperatures.axes_labels[1])
-plt.title(fd_temperatures.dataset_label)
+fig, ax = fd_temperatures.plot()
+ax[0].legend(handles=patches)
 
 #############################################################################################
 # The MS-Plot is generated specifying the tones of the colors defined in the default colormap.
@@ -75,16 +70,12 @@ points, outliers = magnitude_shape_plot(fd_temperatures, color = color, outlierc
 outliers[outliers == 0] = color
 outliers[outliers == 1] = outliercol
 
-colors = np.zeros((fd_temperatures.nsamples, 4))
 colors = colormap(outliers)
+matplotlib.rcParams['axes.prop_cycle'] = cycler(color=colors)
 
 plt.figure()
-for i in range(fd_temperatures.nsamples):
-    plt.plot(fd_temperatures.sample_points[0], fd_temperatures.data_matrix[i], c=colors[i])
+fd_temperatures.plot()
 
-plt.xlabel(fd_temperatures.axes_labels[0])
-plt.ylabel(fd_temperatures.axes_labels[1])
-plt.title(fd_temperatures.dataset_label)
 #######################################################################################
 # We can observe that most of the curves  pointed as outliers belong either to the Pacific or
 # Arctic climates which are not the common ones found in Canada. The Pacific temperatures
@@ -95,11 +86,11 @@ plt.title(fd_temperatures.dataset_label)
 # outliers but in the MS-Plot, they appear further left from the central points. This behaviour
 # can be modified specifying the parameter alpha.
 #
-# Now we use the :func:`Fraiman and Muniz depth measure <fda.depth_measures.Fraiman_Muniz_depth>`
+# Now we use the :func:`Fraiman and Muniz depth measure <fda.depth_measures.fraiman_muniz_depth>`
 # in the MS-Plot.
 
 plt.figure()
-points, outliers = magnitude_shape_plot(fd_temperatures, depth_method = Fraiman_Muniz_depth,
+points, outliers = magnitude_shape_plot(fd_temperatures, depth_method = fraiman_muniz_depth,
                                         color = color, outliercol = outliercol)
 #######################################################################################
 # We can observe that none of the samples are pointed as outliers. Nevertheless, if we group them
@@ -116,15 +107,8 @@ colors_groups = colormap(outliers)
 
 plt.figure()
 plt.scatter(points[:, 0], points[:, 1], c=colors_groups)
+fd_temperatures.set_labels(fig)
 
-plt.xlabel(fd_temperatures.axes_labels[0])
-plt.ylabel(fd_temperatures.axes_labels[1])
-plt.title(fd_temperatures.dataset_label)
-
+matplotlib.rcParams['axes.prop_cycle'] = cycler(color=colors_groups)
 plt.figure()
-for i in range(fd_temperatures.nsamples):
-    plt.plot(fd_temperatures.sample_points[0], fd_temperatures.data_matrix[i], c=colors_groups[i])
-
-plt.xlabel(fd_temperatures.axes_labels[0])
-plt.ylabel(fd_temperatures.axes_labels[1])
-plt.title(fd_temperatures.dataset_label)
+fd_temperatures.plot()

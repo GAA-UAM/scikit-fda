7a04b1f1f54668058984021205e6ca124f90fc76
vnmabus <vnmabus@gmail.com>
2019-06-21 13:21:23 +0200

    * Add leave one out cross validation test * Extract the common code of the smoothers to the abstract parent class

--- tests/test_smoothing.py
+++ tests/test_smoothing.py
@@ -1,6 +1,11 @@
 import unittest
 from skfda._utils import _check_estimator
+import skfda
 import skfda.preprocessing.smoothing.kernel_smoothers as kernel_smoothers
+import skfda.preprocessing.smoothing.validation as validation
+
+import numpy as np
+import sklearn
 
 
 class TestSklearnEstimators(unittest.TestCase):
@@ -13,3 +18,49 @@ class TestSklearnEstimators(unittest.TestCase):
 
     def test_knn(self):
         _check_estimator(kernel_smoothers.KNeighborsSmoother)
+
+
+class _LinearSmootherLeaveOneOutScorerAlternative():
+    r"""Alternative implementation of the LinearSmootherLeaveOneOutScorer"""
+
+    def __call__(self, estimator, X, y):
+        estimator_clone = sklearn.base.clone(estimator)
+
+        estimator_clone._cv = True
+        y_est = estimator_clone.fit_transform(X)
+
+        return -np.mean((y.data_matrix[..., 0] - y_est.data_matrix[..., 0])**2)
+
+
+class TestLeaveOneOut(unittest.TestCase):
+
+    def _test_generic(self, estimator_class):
+        loo_scorer = validation.LinearSmootherLeaveOneOutScorer()
+        loo_scorer_alt = _LinearSmootherLeaveOneOutScorerAlternative()
+        x = np.linspace(-2, 2, 5)
+        fd = skfda.FDataGrid(x ** 2, x)
+
+        estimator = estimator_class()
+
+        grid = validation.optimize_smoothing_parameter(
+            fd, [2, 3],
+            smoothing_method=estimator,
+            cv_method=loo_scorer)
+        score = np.array(grid.cv_results_['mean_test_score'])
+
+        grid_alt = validation.optimize_smoothing_parameter(
+            fd, [2, 3],
+            smoothing_method=estimator,
+            cv_method=loo_scorer_alt)
+        score_alt = np.array(grid_alt.cv_results_['mean_test_score'])
+
+        np.testing.assert_array_almost_equal(score, score_alt)
+
+    def test_nadaraya_watson(self):
+        self._test_generic(kernel_smoothers.NadarayaWatsonSmoother)
+
+    def test_local_linear_regression(self):
+        self._test_generic(kernel_smoothers.LocalLinearRegressionSmoother)
+
+    def test_knn(self):
+        self._test_generic(kernel_smoothers.KNeighborsSmoother)

3cac9a04ea9122db22f87e488763c4520ffaaa28
vnmabus <vnmabus@gmail.com>
2019-06-27 19:23:03 +0200

    Add Gaussian covariance and ABC class with rich representation

--- skfda/misc/covariances.py
+++ skfda/misc/covariances.py
@@ -1,6 +1,14 @@
 import numbers
 
 import numpy as np
+import abc
+import matplotlib
+
+from .._utils import _figure_to_svg
+
+
+def _squared_norms(x, y):
+    return ((x[np.newaxis, :, :] - y[:, np.newaxis, :]) ** 2).sum(2)
 
 
 def _transform_to_2d(t):
@@ -36,63 +44,135 @@ def _execute_covariance(covariance, x, y):
         return result
 
 
-class Brownian():
-    """Brownian covariance"""
+class Covariance(abc.ABC):
+    """Abstract class for covariance functions"""
+
+    @abc.abstractmethod
+    def __call__(self, x, y):
+        pass
+
+    def heatmap(self):
+        x = np.linspace(-1, 1, 1000)
+
+        cov_matrix = self(x, x)
+
+        fig = matplotlib.figure.Figure()
+        ax = fig.add_subplot(1, 1, 1)
+        ax.imshow(cov_matrix, extent=[-1, 1, 1, -1])
+        ax.set_title("Covariance function in [-1, 1]")
+
+        return fig, ax
+
+    def _sample_trajectories_plot(self):
+        from ..datasets import make_gaussian_process
+
+        fd = make_gaussian_process(cov=self)
+        fig, ax = fd.plot()
+        ax[0].set_title("Sample trajectories")
+        return fig, ax
+
+    def __repr__(self):
+
+        params = ', '.join(f'{n}={getattr(self, n)}'
+                           for n, _ in self._parameters)
+
+        return (f"{self.__module__}.{type(self).__qualname__}("
+                f"{params}"
+                f")")
+
+    def _latex_content(self):
+        params = ''.join(fr'{l} &= {getattr(self, n)} \\'
+                         for n, l in self._parameters)
+
+        return (fr"{self._latex_formula} \\"
+                r"\text{where:}"
+                r"\begin{aligned}"
+                fr"\qquad{params}"
+                r"\end{aligned}")
+
+    def _repr_latex_(self):
+        return fr"\(\displaystyle {self._latex_content()}\)"
+
+    def _repr_html_(self):
+        fig, _ = self.heatmap()
+        heatmap = _figure_to_svg(fig)
+
+        fig, _ = self._sample_trajectories_plot()
+        sample_trajectories = _figure_to_svg(fig)
+
+        row_style = 'style="display: flex; display:table-row"'
+
+        def column_style(percent):
+            return (f'style="flex: {percent}%; display: table-cell; '
+                    f'vertical-align: middle"')
+
+        html = f"""
+        <div {row_style}>
+            <div {column_style(50)}>
+            \\[{self._latex_content()}\\]
+            </div>
+        </div>
+        <div {row_style}>
+            <div {column_style(50)}>
+            {sample_trajectories}
+            </div>
+            <div {column_style(50)}>
+            {heatmap}
+            </div>
+        </div>
+        """
+
+        return html
+
+
+class Brownian(Covariance):
+    """Brownian covariance function."""
+
+    _latex_formula = (r"K(x, y) = \sigma^2 \frac{|x - \mathcal{O}| + "
+                      r"|y - \mathcal{O}| - |x-y|}{2}")
+
+    _parameters = [("variance", r"\sigma^2"),
+                   ("origin", r"\mathcal{O}")]
 
     def __init__(self, *, variance: float = 1., origin: float = 0.):
         self.variance = variance
         self.origin = origin
 
     def __call__(self, x, y):
-        """Brownian covariance function"""
-        x = np.asarray(x) - self.origin
-        y = np.asarray(y) - self.origin
+        x = _transform_to_2d(x) - self.origin
+        y = _transform_to_2d(y) - self.origin
 
         return self.variance * (np.abs(x) + np.abs(y.T) - np.abs(x - y.T)) / 2
 
-    def __repr__(self):
-        return (f"{self.__module__}.{type(self).__qualname__}("
-                f"variance={self.variance}, origin={self.origin})")
 
-    def _repr_latex_(self):
-        return (r"\[K(x, y) = \sigma^2 \frac{|x - \mathcal{O}| + "
-                r"|y - \mathcal{O}| - |x-y|}{2}\]"
-                "where:"
-                r"\begin{align*}"
-                fr"\qquad\sigma^2 &= {self.variance} \\"
-                fr"\mathcal{{O}} &= {self.origin} \\"
-                r"\end{align*}")
+class Linear(Covariance):
+    """Linear covariance function."""
 
+    _latex_formula = r"K(x, y) = \sigma^2 (x^T y + c)"
 
-class Linear():
-    """Linear covariance"""
+    _parameters = [("variance", r"\sigma^2"),
+                   ("intercept", r"c")]
 
     def __init__(self, *, variance: float = 1., intercept: float = 0.):
         self.variance = variance
         self.intercept = intercept
 
     def __call__(self, x, y):
-        """Brownian covariance function"""
-        x = np.asarray(x)
-        y = np.asarray(y)
+        x = _transform_to_2d(x)
+        y = _transform_to_2d(y)
 
         return self.variance * (x @ y.T + self.intercept)
 
-    def __repr__(self):
-        return (f"{self.__module__}.{type(self).__qualname__}("
-                f"variance={self.variance}, intercept={self.intercept})")
 
-    def _repr_latex_(self):
-        return (r"\[K(x, y) = \sigma^2 (x^T y + c)\]"
-                "where:"
-                r"\begin{align*}"
-                fr"\qquad\sigma^2 &= {self.variance} \\"
-                fr"c &= {self.intercept} \\"
-                r"\end{align*}")
+class Polynomial(Covariance):
+    """Polynomial covariance function."""
 
+    _latex_formula = r"K(x, y) = \sigma^2 (\alpha x^T y + c)^d"
 
-class Polynomial():
-    """Polynomial covariance"""
+    _parameters = [("variance", r"\sigma^2"),
+                   ("intercept", r"c"),
+                   ("slope", r"\alpha"),
+                   ("degree", r"d")]
 
     def __init__(self, *, variance: float = 1., intercept: float = 0.,
                  slope: float = 1., degree: float = 2.):
@@ -102,24 +182,30 @@ class Polynomial():
         self.degree = degree
 
     def __call__(self, x, y):
-        """Brownian covariance function"""
-        x = np.asarray(x)
-        y = np.asarray(y)
+        x = _transform_to_2d(x)
+        y = _transform_to_2d(y)
 
         return self.variance * (self.slope * x @ y.T
                                 + self.intercept)**self.degree
 
-    def __repr__(self):
-        return (f"{self.__module__}.{type(self).__qualname__}("
-                f"variance={self.variance}, intercept={self.intercept}, "
-                f"slope={self.slope}, degree={self.degree})")
 
-    def _repr_latex_(self):
-        return (r"\[K(x, y) = \sigma^2 (\alpha x^T y + c)^d\]"
-                "where:"
-                r"\begin{align*}"
-                fr"\qquad\sigma^2 &= {self.variance} \\"
-                fr"\alpha &= {self.slope} \\"
-                fr"c &= {self.intercept} \\"
-                fr"d &= {self.degree} \\"
-                r"\end{align*}")
+class Gaussian(Covariance):
+    """Gaussian covariance function."""
+
+    _latex_formula = (r"K(x, y) = \sigma^2 \exp\left(\frac{||x - y||^2}{2l^2}"
+                      r"\right)")
+
+    _parameters = [("variance", r"\sigma^2"),
+                   ("length_scale", r"l")]
+
+    def __init__(self, *, variance: float = 1., length_scale: float = 1.):
+        self.variance = variance
+        self.length_scale = length_scale
+
+    def __call__(self, x, y):
+        x = _transform_to_2d(x)
+        y = _transform_to_2d(y)
+
+        x_y = _squared_norms(x, y)
+
+        return self.variance * np.exp(-x_y / (2 * self.length_scale**2))

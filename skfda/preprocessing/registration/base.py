1adc95fbae2047bc0226b912e88d32c872012ec9
pablomm <pablo.marcosm@protonmail.com>
2019-10-11 18:56:28 +0200

    Fix score method of the registration transformers

--- skfda/preprocessing/registration/base.py
+++ skfda/preprocessing/registration/base.py
@@ -20,8 +20,9 @@ class RegistrationTransformer(ABC, BaseEstimator, TransformerMixin):
 
         where :math:`\text{MSE}_{total}` is the mean squared error and
         :math:`\text{MSE}_{phase}` is the mean squared error due to the phase
-        explained by the registration procedure. See :func:`mse_decomposition`
-        for a detailed explanation.
+        explained by the registration procedure. See
+        :class:`~.validation.AmplitudePhaseDecomposition` for a detailed
+        explanation.
 
         Args:
             X (FData): Functional data to be registered
@@ -31,13 +32,12 @@ class RegistrationTransformer(ABC, BaseEstimator, TransformerMixin):
             float.
 
         See also:
-            :class:`RegistrationScorer <RegistrationScorer>`
-            :func:`mse_r_squared <mse_r_squared>`
-            :func:`least_squares <least_squares>`
-            :func:`sobolev_least_squares <sobolev_least_squares>`
-            :func:`pairwise_correlation <pairwise_correlation>`
+            :class:`~.validation.AmplitudePhaseDecomposition`
+            :class:`~.validation.LeastSquares`
+            :class:`~.validation.SobolevLeastSquares`
+            :class:`~.validation.PairwiseCorrelation`
 
         """
-        from .validation import RegistrationScorer
+        from .validation import AmplitudePhaseDecomposition
 
-        return RegistrationScorer()(self, X, y)
+        return AmplitudePhaseDecomposition()(self, X, y)

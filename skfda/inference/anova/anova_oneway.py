0a1b472915268aa71a4787360a079ad034b6d874
David García Fernández <david.garcia03@estudiante.uam.es>
2019-12-11 00:21:57 +0100

    Implemented fda.usc version.
    Signed-off-by: David García Fernández <david.garcia03@estudiante.uam.es>

--- skfda/inference/anova/anova_oneway.py
+++ skfda/inference/anova/anova_oneway.py
@@ -1,90 +1,77 @@
 import numpy as np
-from skfda.misc.metrics import lp_distance
+from skfda.misc.metrics import norm_lp, lp_distance
 from skfda.representation import FDataGrid
 from skfda.datasets import make_gaussian_process
 
 
 def vn_statistic(fd_means, sizes):
-    # Calculating weighted sum of L2 distances between means
-    distances_m = np.tril(lp_distance(fd_means, fd_means))  # lp_distance not working as expected
-    # Calculating square of the distances and summing by groups
-    distances_group = np.sum(np.multiply(distances_m, distances_m), axis=1)
-    # Weighted sum
-    return sum(distances_group * sizes)
+    k = fd_means.data_matrix.shape[0]
+    v_n = 0
+    for i in range(k):
+        for j in range(i + 1, k):
+            # v1 = np.squeeze(fd_means[i].data_matrix[0])
+            # v2 = np.squeeze(fd_means[j].data_matrix[0])
+            # v_n += sizes[i] * np.linalg.norm(v1 - v2) ** 2
+            v_n += sizes[i] * norm_lp(fd_means[i] - fd_means[j]) ** 2
+    return v_n
 
 
 def anova_bootstrap(fd_grouped, n_sim):
-    if len(fd_grouped) < 1:
-        return
+    assert len(fd_grouped) > 0
 
     m = fd_grouped[0].ncol
+    samples = fd_grouped[0].sample_points
     k = len(fd_grouped)
     start, stop = fd_grouped[0].domain_range[0]
+    sizes = [fd.n_samples for fd in fd_grouped]
 
-    # Estimating covariances
+    # Estimating covariances for each group
     k_est = [np.squeeze(fd.cov().data_matrix[0]) for fd in fd_grouped]
 
-    # Simulation
-    simulation = np.empty((0, k, m))
+    l_vector = []
     for l in range(n_sim):
-        sim_l = np.empty((0, m))
+        sim = FDataGrid(np.empty((0, m)), sample_points=samples)
         for i, fd in enumerate(fd_grouped):
-            process = make_gaussian_process(n_samples=1, n_features=m, start=start,
-                                            stop=stop, cov=k_est[i])
-            sim_l = np.append(sim_l, [np.squeeze(process.data_matrix)], axis=0)
-        simulation = np.append(simulation, [sim_l], axis=0)
-    return simulation
+            process = make_gaussian_process(fd.n_samples, n_features=m, start=start, stop=stop, cov=k_est[i])
+            sim = sim.concatenate(process.mean())
+        # l_vector.append(v_hat_statistic(sim, sizes))
+        l_vector.append(v_usc(sim))
+    return l_vector
 
 
-def vn_temp(fd_means, sizes):
-
-    means = []
-    for f in fd_means.data_matrix:
-        means.append(FDataGrid(np.squeeze(f), sample_points=np.squeeze(fd_means.sample_points[0])))
-
-    v = 0
-
-    for i in range(len(means)):
-        for j in range(i + 1, len(means)):
-            v += sizes[i] * lp_distance(means[i], means[j]) ** 2
-
-    return v
+def v_hat_statistic(values, sizes):
+    k = len(values)
+    v_hat = 0
+    for i in range(k):
+        for j in range(i + 1, k):
+            # v1 = np.squeeze(values[i].data_matrix[0])
+            # v2 = np.squeeze(values[j].data_matrix[0])
+            # v_hat += np.linalg.norm(v1 - v2 * np.sqrt(sizes[i] / sizes[j]))**2
+            v_hat += norm_lp(values[i] - values[j] * np.sqrt(sizes[i] / sizes[j])) ** 2
+    return v_hat
 
 
-def v_gorros(simulaciones, sizes):
-    distr = []
-    for s in simulaciones:
-        v = 0
-        for i in range(len(s)):
-            for j in range(i + 1, len(s)):
-                v += np.linalg.norm(s[i] - s[j] * np.sqrt(sizes[i] / sizes[j])) ** 2
-        distr.append(v)
-    return np.array(distr)
+def func_oneway(*args, n_sim=2000):
+    # TODO Check grids
+    assert len(args) > 0
 
+    fd_groups = args
+    fd_means = fd_groups[0].mean()
+    for fd in fd_groups[1:]:
+        fd_means = fd_means.concatenate(fd.mean())
 
-def func_oneway(fdata, groups, n_sim):
-    # Obtaining the different group labels
-    group_set = np.unique(groups)
-
-    fd_groups = []
-    means = None
-    for group in group_set:
-        # Creating an independent FDataGrid for each group
-        indices = np.where(groups == group)[0]
-        fd = FDataGrid(np.squeeze(np.take(fdata.data_matrix, indices, axis=0)),
-                       sample_points=fdata.sample_points)
-        fd_groups.append(fd)
-        # Creating FDataGrid with the means of each group
-        if not means:
-            means = fd.mean()
-        else:
-            means = means.concatenate(fd.mean())
-
-    # vn = vn_statistic(means, [fd.n_samples for fd in fd_groups])
-    vn = vn_temp(means, [fd.n_samples for fd in fd_groups])
-
+    # vn = vn_statistic(fd_means, [fd.n_samples for fd in fd_groups])
+    vn = v_usc(fd_means)
     simulation = anova_bootstrap(fd_groups, n_sim)
-    v = v_gorros(simulation, [10, 10, 10])
-    p_value = len(np.where(v >= vn)[0]) / len(v)
+    p_value = len(np.where(simulation >= vn)[0]) / len(simulation)
 
-    return p_value, vn, v
+    return p_value, vn, simulation
+
+
+def v_usc(values):
+    k = len(values)
+    v = 0
+    for i in range(k):
+        for j in range(i + 1, k):
+            v += norm_lp(values[i] - values[j])
+    return v

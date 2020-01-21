3f4ac36d5fc3eb20c64c6a86c09440f4cc03231f
David García Fernández <david.garcia03@estudiante.uam.es>
2020-01-21 11:52:15 +0100

    Anova bootstrap working.

--- skfda/inference/anova/anova_oneway_aux.py
+++ skfda/inference/anova/anova_oneway_aux.py
@@ -1,56 +1,11 @@
+from skfda import FDataGrid
 import numpy as np
-from skfda.misc.metrics import norm_lp, lp_distance
-from skfda.representation import FDataGrid
+from skfda.inference.anova.anova_oneway import func_oneway, func_oneway_usc
 from skfda.datasets import make_gaussian_process
+from matplotlib import pyplot as plt
 
+m = 25
+n = 1
 
-def v_usc(values):
-    k = len(values)
-    v = 0
-    for i in range(k):
-        for j in range(i + 1, k):
-            v += norm_lp(values[i] - values[j])
-    return v
-
-
-def anova_bootstrap_usc(fd_grouped, n_sim):
-    assert len(fd_grouped) > 0
-
-    m = fd_grouped[0].ncol
-    samples = fd_grouped[0].sample_points
-    start, stop = fd_grouped[0].domain_range[0]
-    sizes = [fd.n_samples for fd in fd_grouped]
-
-    # Estimating covariances for each group
-    k_est = [fd.cov().data_matrix[0, ..., 0] for fd in fd_grouped]
-
-    l_vector = []
-    for l in range(n_sim):
-        sim = FDataGrid(np.empty((0, m)), sample_points=samples)
-        for i, fd in enumerate(fd_grouped):
-            process = make_gaussian_process(fd.n_samples, n_features=m, start=start, stop=stop, cov=k_est[i])
-            sim = sim.concatenate(process.mean())
-        l_vector.append(v_usc(sim))
-
-    return l_vector
-
-
-def oneway(*args, n_sim=2000):
-
-    # TODO Check grids
-
-    assert len(args) > 0
-
-    fd_groups = args
-    fd_means = fd_groups[0].mean()
-    for fd in fd_groups[1:]:
-        fd_means = fd_means.concatenate(fd.mean())
-
-    vn = v_usc(fd_means)
-
-    simulation = anova_bootstrap_usc(fd_groups, n_sim=n_sim)
-    p_value = len(np.where(simulation >= vn)[0]) / len(simulation)
-
-    return p_value, vn, simulation
-
-
+process = make_gaussian_process(n, n_features=m)
+print(process.mean().data_matrix)
--- skfda/inference/anova/anova_simulation.py
+++ skfda/inference/anova/anova_simulation.py
@@ -15,34 +15,33 @@ start = 0
 stop = 1
 
 n_levels = 3
-n_samples = 100
+n_samples = 10
 
 t = np.linspace(start, stop, scale)
 
 sigmas = np.array([0, 0.2, 1, 1.8, 2.6, 3.4, 4.2, 5])
-sigmas_star = sigmas * scale
+sigmas_star = sigmas / scale
 
 # Case M1
-mean1 = t * (1 - t)
-mean2 = t * (1 - t)
-mean3 = t * (1 - t)
+# mean1 = t * (1 - t)
+# mean2 = t * (1 - t)
+# mean3 = t * (1 - t)
+
+mean1 = t * (1 - t) ** 5
+mean2 = t ** 2 * (1 - t) ** 4
+mean3 = t ** 3 * (1 - t) ** 3
 
 fd_means = FDataGrid([mean1, mean2, mean3])
-fd_means.plot()
-plt.show()
 
 p = []
-reps = 500
+reps = 20
 
 for i in range(reps):
-    if i % 100 == 1 and i != 1:
-        print(np.mean(p))
-        p = []
 
     print('Simulation {}...'.format(i + 1))
-    samples1 = generate_samples_independent(mean1, sigmas_star[1], n_samples)
-    samples2 = generate_samples_independent(mean2, sigmas_star[1], n_samples)
-    samples3 = generate_samples_independent(mean3, sigmas_star[1], n_samples)
+    samples1 = generate_samples_independent(mean1, sigmas_star[3], n_samples)
+    samples2 = generate_samples_independent(mean2, sigmas_star[3], n_samples)
+    samples3 = generate_samples_independent(mean3, sigmas_star[3], n_samples)
 
     # Storing in FDataGrid
     fd_1 = FDataGrid(samples1, sample_points=t, dataset_label="Process 1")
@@ -50,6 +49,9 @@ for i in range(reps):
     fd_3 = FDataGrid(samples3, sample_points=t, dataset_label="Process 3")
     fd_total = fd_1.concatenate(fd_2.concatenate(fd_3))
 
-    p.append(func_oneway(fd_1, fd_2, fd_3, n_sim=2000)[0])
+    anova = func_oneway(fd_1, fd_2, fd_3, n_sim=2000)
+    print(anova)
+    p.append(anova[0])
 
 print(np.mean(p))
+

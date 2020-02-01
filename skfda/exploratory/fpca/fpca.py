fa640070d029b39c66b93fe618fce344e82c1f30
Yujian Hong <yujian.hong@estudiante.uam.es>
2020-02-01 15:42:43 +0100

    Creating tests

--- skfda/exploratory/fpca/fpca.py
+++ skfda/exploratory/fpca/fpca.py
@@ -2,44 +2,56 @@ import numpy as np
 from abc import ABC, abstractmethod
 from skfda.representation.basis import FDataBasis
 from skfda.representation.grid import FDataGrid
-from sklearn.decomposition import PCA
 from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.decomposition import PCA
 
 
 class FPCA(ABC, BaseEstimator, ClassifierMixin):
-    """Defines the common structure shared between classes that do functional principal component analysis
+    # TODO doctring
+    # TODO doctext
+    # TODO directory examples create test
+    """
+    Defines the common structure shared between classes that do functional
+    principal component analysis
 
     Attributes:
-        n_components (int): number of principal components to obtain from functional principal component analysis
-        centering (bool): if True then calculate the mean of the functional data object and center the data first
-        svd (bool): if True then we use svd to obtain the principal components. Otherwise we use eigenanalysis
-        components (FDataGrid or FDataBasis): this contains the principal components either in a basis form or
-            discretized form
-        component_values (array_like): this contains the values (eigenvalues) associated with the principal components
+        n_components (int): number of principal components to obtain from
+            functional principal component analysis
+        centering (bool): if True then calculate the mean of the functional data
+            object and center the data first
+        components (FDataGrid or FDataBasis): this contains the principal
+            components either in a basis form or discretized form
+        component_values (array_like): this contains the values (eigenvalues)
+            associated with the principal components
 
     """
 
     def __init__(self, n_components=3, centering=True):
-        """ FPCA constructor
+        """
+        FPCA constructor
         Args:
-            n_components (int): number of principal components to obtain from functional principal component analysis
-            centering (bool): if True then calculate the mean of the functional data object and center the data first.
-                Defaults to True
-            svd (bool): if True then we use svd to obtain the principal components. Otherwise we use eigenanalysis.
-                Defaults to True as svd is usually more efficient
+            n_components (int): number of principal components to obtain from
+                functional principal component analysis
+            centering (bool): if True then calculate the mean of the functional
+                data object and center the data first. Defaults to True
         """
         self.n_components = n_components
         self.centering = centering
         self.components = None
         self.component_values = None
+        self.pca = PCA(n_components=self.n_components)
 
     @abstractmethod
     def fit(self, X, y=None):
-        """Computes the n_components first principal components and saves them inside the FPCA object.
+        """
+        Computes the n_components first principal components and saves them
+        inside the FPCA object.
 
             Args:
-                X (FDataGrid or FDataBasis): the functional data object to be analysed
-                y (None, not used): only present for convention of a fit function
+                X (FDataGrid or FDataBasis):
+                    the functional data object to be analysed
+                y (None, not used):
+                    only present for convention of a fit function
 
             Returns:
                 self (object)
@@ -48,26 +60,35 @@ class FPCA(ABC, BaseEstimator, ClassifierMixin):
 
     @abstractmethod
     def transform(self, X, y=None):
-        """Computes the n_components first principal components score and returns them.
+        """
+        Computes the n_components first principal components score and returns
+        them.
 
             Args:
-                X (FDataGrid or FDataBasis): the functional data object to be analysed
-                y (None, not used): only present for convention of a fit function
+                X (FDataGrid or FDataBasis):
+                    the functional data object to be analysed
+                y (None, not used):
+                    only present for convention of a fit function
 
             Returns:
-                (array_like): the scores of the data with reference to the principal components
+                (array_like): the scores of the data with reference to the
+                principal components
         """
         pass
 
     def fit_transform(self, X, y=None):
-        """Computes the n_components first principal components and their scores and returns them.
-
+        """
+        Computes the n_components first principal components and their scores
+        and returns them.
             Args:
-                X (FDataGrid or FDataBasis): the functional data object to be analysed
-                y (None, not used): only present for convention of a fit function
+                X (FDataGrid or FDataBasis):
+                    the functional data object to be analysed
+                y (None, not used):
+                    only present for convention of a fit function
 
             Returns:
-                (array_like): the scores of the data with reference to the principal components
+                (array_like): the scores of the data with reference to the
+                principal components
         """
         self.fit(X, y)
         return self.transform(X, y)
@@ -77,18 +98,19 @@ class FPCABasis(FPCA):
 
     def __init__(self, n_components=3, components_basis=None, centering=True):
         super().__init__(n_components, centering)
-        # component_basis is the basis that we want to use for the principal components
+        # basis that we want to use for the principal components
         self.components_basis = components_basis
 
     def fit(self, X: FDataBasis, y=None):
-        # initialize pca
-        self.pca = PCA(n_components=self.n_components)
 
-        # if centering is True then substract the mean function to each function in FDataBasis
+        # check that the parameter is
+
+        # if centering is True then subtract the mean function to each function
+        # in FDataBasis
         if self.centering:
             meanfd = X.mean()
             # consider moving these lines to FDataBasis as a centering function
-            # substract from each row the mean coefficient matrix
+            # subtract from each row the mean coefficient matrix
             X.coefficients -= meanfd.coefficients
 
         # for reference, X.coefficients is the C matrix
@@ -96,7 +118,8 @@ class FPCABasis(FPCA):
 
         # setup principal component basis if not given
         if self.components_basis:
-            # if the principal components are in the same basis, this is essentially the gram matrix
+            # if the principal components are in the same basis, this is
+            # essentially the gram matrix
             g_matrix = self.components_basis.gram_matrix()
             j_matrix = X.basis.inner_product(self.components_basis)
         else:
@@ -104,6 +127,10 @@ class FPCABasis(FPCA):
             g_matrix = self.components_basis.gram_matrix()
             j_matrix = g_matrix
 
+        # make g matrix symmetric, referring to Ramsay's implementation
+        g_matrix = (g_matrix + np.transpose(g_matrix))/2
+
+        # obtain triangulation using cholesky
         l_matrix = np.linalg.cholesky(g_matrix)
 
         # L^{-1}
@@ -112,15 +139,15 @@ class FPCABasis(FPCA):
         # The following matrix is needed: L^{-1}*J^T
         l_inv_j_t = l_matrix_inv @ np.transpose(j_matrix)
 
-        # TODO make the final matrix symmetric, not necessary as the final matrix is not a square matrix?
-
-        # the final matrix, C(L-1Jt)t for svd or (L-1Jt)-1CtC(L-1Jt)t for eigen analysis
-        final_matrix = X.coefficients @ np.transpose(l_inv_j_t) / np.sqrt(n_samples)
+        # the final matrix, C(L-1Jt)t for svd or (L-1Jt)-1CtC(L-1Jt)t for PCA
+        final_matrix = X.coefficients @ np.transpose(l_inv_j_t) / \
+            np.sqrt(n_samples)
 
         self.pca.fit(final_matrix)
         self.component_values = self.pca.singular_values_ ** 2
         self.components = X.copy(basis=self.components_basis,
-                                 coefficients=self.pca.components_ @ l_matrix_inv)
+                                 coefficients=self.pca.components_
+                                 @ l_matrix_inv)
         """
         if self.svd:
             # vh contains the eigenvectors transposed
@@ -167,16 +194,15 @@ class FPCADiscretized(FPCA):
 
     # noinspection PyPep8Naming
     def fit(self, X: FDataGrid, y=None):
-        # initialize pca module
-        self.pca = PCA(n_components=self.n_components)
 
         # data matrix initialization
         fd_data = np.squeeze(X.data_matrix)
 
-        # obtain the number of samples and the number of points of descretization
+        # get the number of samples and the number of points of descretization
         n_samples, n_points_discretization = fd_data.shape
 
-        # if centering is True then subtract the mean function to each function in FDataBasis
+        # if centering is True then subtract the mean function to each function
+        # in FDataBasis
         if self.centering:
             meanfd = X.mean()
             # consider moving these lines to FDataBasis as a centering function
@@ -186,10 +212,12 @@ class FPCADiscretized(FPCA):
         # establish weights for each point of discretization
         if not self.weights:
             # sample_points is a list with one array in the 1D case
-            # in trapezoidal rule, suppose \deltax_k = x_k - x_{k-1}, the weight vector is as follows:
-            # [\deltax_1/2, \deltax_1/2 + \deltax_2/2, \deltax_2/2 + \deltax_3/2, ... , \deltax_n/2]
+            # in trapezoidal rule, suppose \deltax_k = x_k - x_{k-1}, the weight
+            # vector is as follows: [\deltax_1/2, \deltax_1/2 + \deltax_2/2,
+            # \deltax_2/2 + \deltax_3/2, ... , \deltax_n/2]
             differences = np.diff(X.sample_points[0])
-            self.weights = [sum(differences[i:i + 2]) / 2 for i in range(len(differences))]
+            self.weights = [sum(differences[i:i + 2]) / 2 for i in
+                            range(len(differences))]
             self.weights = np.concatenate(([differences[0] / 2], self.weights))
 
         weights_matrix = np.diag(self.weights)
@@ -200,7 +228,7 @@ class FPCADiscretized(FPCA):
         final_matrix = fd_data @ np.sqrt(weights_matrix) / np.sqrt(n_samples)
         self.pca.fit(final_matrix)
         self.components = X.copy(data_matrix=self.pca.components_)
-        self.component_values = self.pca.singular_values_**2
+        self.component_values = self.pca.singular_values_ ** 2
 
         """
         if self.svd:
@@ -230,5 +258,7 @@ class FPCADiscretized(FPCA):
         return self
 
     def transform(self, X, y=None):
-        # in this case its the coefficient matrix multiplied by the principal components as column vectors
-        return np.squeeze(X.data_matrix) @ np.transpose(np.squeeze(self.components.data_matrix))
+        # in this case its the coefficient matrix multiplied by the principal
+        # components as column vectors
+        return np.squeeze(X.data_matrix) @ np.transpose(
+            np.squeeze(self.components.data_matrix))
--- tests/test_fpca.py
+++ tests/test_fpca.py
@@ -1,81 +1,25 @@
 import unittest
 
 import numpy as np
-from skfda import FDataGrid, FDataBasis
-from skfda.representation.basis import Fourier
-from skfda.preprocessing.dim_reduction.projection import FPCABasis, FPCAGrid
-from skfda.datasets import fetch_weather
+from skfda import FDataGrid
+from skfda.exploratory.fpca import FPCABasis, FPCADiscretized
+from skfda.datasets import fetch_growth, fetch_weather
 
 
-class FPCATestCase(unittest.TestCase):
+def fetch_weather_temp_only():
+    weather_dataset = fetch_weather()
+    fd_data = weather_dataset['data']
+    fd_data.data_matrix = fd_data.data_matrix[:, :, :1]
+    fd_data.axes_labels = fd_data.axes_labels[:-1]
+    return fd_data
 
-    def test_basis_fpca_fit_attributes(self):
+class MyTestCase(unittest.TestCase):
+    def test_basis_fpca_fit(self):
         fpca = FPCABasis()
         with self.assertRaises(AttributeError):
             fpca.fit(None)
 
-        basis = Fourier(n_basis=1)
-        # check that if n_components is bigger than the number of samples then
-        # an exception should be thrown
-        fd = FDataBasis(basis, [[0.9]])
-        with self.assertRaises(AttributeError):
-            fpca.fit(fd)
 
-        # check that n_components must be smaller than the number of elements
-        # of target basis
-        fd = FDataBasis(basis, [[0.9], [0.7], [0.5]])
-        with self.assertRaises(AttributeError):
-            fpca.fit(fd)
-
-    def test_discretized_fpca_fit_attributes(self):
-        fpca = FPCAGrid()
-        with self.assertRaises(AttributeError):
-            fpca.fit(None)
-
-        # check that if n_components is bigger than the number of samples then
-        # an exception should be thrown
-        fd = FDataGrid([[0.5], [0.1]], sample_points=[0])
-        with self.assertRaises(AttributeError):
-            fpca.fit(fd)
-
-        # check that n_components must be smaller than the number of attributes
-        # in the FDataGrid object
-        fd = FDataGrid([[0.9], [0.7], [0.5]], sample_points=[0])
-        with self.assertRaises(AttributeError):
-            fpca.fit(fd)
-
-    def test_basis_fpca_fit_result(self):
-
-        n_basis = 9
-        n_components = 3
-
-        fd_data = fetch_weather()['data'].coordinates[0]
-        fd_data = FDataGrid(np.squeeze(fd_data.data_matrix),
-                            np.arange(0.5, 365, 1))
-
-        # initialize basis data
-        basis = Fourier(n_basis=9, domain_range=(0, 365))
-        fd_basis = fd_data.to_basis(basis)
-
-        fpca = FPCABasis(n_components=n_components)
-        fpca.fit(fd_basis)
-
-        # results obtained using Ramsay's R package
-        results = [[0.9231551, 0.1364966, 0.3569451, 0.0092012, -0.0244525,
-                    -0.02923873, -0.003566887, -0.009654571, -0.0100063],
-                   [-0.3315211, -0.0508643, 0.89218521, 0.1669182, 0.2453900,
-                    0.03548997, 0.037938051, -0.025777507, 0.008416904],
-                   [-0.1379108,  0.9125089, 0.00142045, 0.2657423, -0.2146497,
-                    0.16833314,  0.031509179, -0.006768189, 0.047306718]]
-        results = np.array(results)
-
-        # compare results obtained using this library. There are slight
-        # variations due to the fact that we are in two different packages
-        for i in range(n_components):
-            if np.sign(fpca.components_.coefficients[i][0]) != np.sign(results[i][0]):
-                results[i, :] *= -1
-        np.testing.assert_allclose(fpca.components_.coefficients, results,
-                                   atol=1e-7)
 
 
 if __name__ == '__main__':

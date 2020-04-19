698438e316d313e3d57782bf79d2a6f592a44d2a
vnmabus <vnmabus@gmail.com>
2020-04-20 01:42:21 +0200

    Fixed staticdispatch.

--- skfda/_utils/__init__.py
+++ skfda/_utils/__init__.py
@@ -3,4 +3,4 @@ from . import constants
 from ._utils import (_list_of_arrays, _coordinate_list,
                      _check_estimator, parameter_aliases,
                      _to_grid, check_is_univariate,
-                     _same_domain, singledispatchmethod)
+                     _same_domain)

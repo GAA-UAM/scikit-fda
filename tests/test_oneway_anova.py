706aa6761fc40b5f171179f480ae0af2b6370abb
davidgarciafer <david.garcia03@estudiante.uam.es>
2020-05-06 22:03:47 +0200

    Fixing travis run of tests.
    Signed-off-by: David García Fernández <david.garcia03@estudiante.uam.es>

--- tests/test_oneway_anova.py
+++ tests/test_oneway_anova.py
@@ -46,7 +46,6 @@ class OnewayAnovaTests(unittest.TestCase):
         self.assertAlmostEqual(v_asymptotic_stat(fd.to_basis(Fourier(
             n_basis=5)), weights), res)
 
-    @pytest.mark.slow
     def test_asymptotic_behaviour(self):
         dataset = fetch_gait()
         fd = dataset['data'].coordinates[1]

a94a5c23430abac92f19db2378a9147f886ca9cd
vnmabus <vnmabus@gmail.com>
2021-01-03 03:19:49 +0100

    Fix style in vector basis.

--- skfda/representation/basis/_vector_basis.py
+++ skfda/representation/basis/_vector_basis.py
@@ -1,10 +1,17 @@
-from typing import Iterable
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Iterable, Tuple, TypeVar, Union
 
 import numpy as np
 import scipy.linalg
 
 from ..._utils import _same_domain
-from ._basis import Basis, Tuple
+from ._basis import Basis
+
+if TYPE_CHECKING:
+    from .. import FDataBasis
+
+T = TypeVar("T", bound='VectorValued')
 
 
 class VectorValued(Basis):
@@ -68,9 +75,11 @@ class VectorValued(Basis):
                 "The basis functions must be scalar valued",
             )
 
-        if any(b.dim_domain != basis_list[0].dim_domain or
-               not _same_domain(b,  basis_list[0])
-               for b in basis_list):
+        if any(
+            b.dim_domain != basis_list[0].dim_domain
+            or not _same_domain(b, basis_list[0])
+            for b in basis_list
+        ):
             raise ValueError(
                 "The basis must all have the same domain "
                 "dimension and range",
@@ -98,70 +107,90 @@ class VectorValued(Basis):
     def _evaluate(self, eval_points: np.ndarray) -> np.ndarray:
         matrix = np.zeros((self.n_basis, len(eval_points), self.dim_codomain))
 
-        n_basis_evaluated = 0
+        n_basis_eval = 0
 
-        basis_evaluations = [b._evaluate(eval_points) for b in self.basis_list]
+        basis_evaluations = [b.evaluate(eval_points) for b in self.basis_list]
 
         for i, ev in enumerate(basis_evaluations):
 
-            matrix[n_basis_evaluated:n_basis_evaluated + len(ev), :, i] = ev
-            n_basis_evaluated += len(ev)
+            matrix[n_basis_eval:n_basis_eval + len(ev), :, i] = ev[..., 0]
+            n_basis_eval += len(ev)
 
         return matrix
 
     def _derivative_basis_and_coefs(
-            self,
-            coefs: np.ndarray,
-            order: int = 1,
-    ) -> Tuple[Basis, np.ndarray]:
+        self: T,
+        coefs: np.ndarray,
+        order: int = 1,
+    ) -> Tuple[T, np.ndarray]:
 
         n_basis_list = [b.n_basis for b in self.basis_list]
         indexes = np.cumsum(n_basis_list)
 
         coefs_per_basis = np.hsplit(coefs, indexes[:-1])
 
-        basis_and_coefs = [b._derivative_basis_and_coefs(
-            c, order=order) for b, c in zip(self.basis_list, coefs_per_basis)]
+        basis_and_coefs = [
+            b._derivative_basis_and_coefs(c, order=order)  # noqa: WPS437
+            for b, c in zip(self.basis_list, coefs_per_basis)
+        ]
 
         new_basis_list, new_coefs_list = zip(*basis_and_coefs)
 
-        new_basis = VectorValued(new_basis_list)
+        new_basis = type(self)(new_basis_list)
         new_coefs = np.hstack(new_coefs_list)
 
         return new_basis, new_coefs
 
-    def _gram_matrix(self):
+    def _gram_matrix(self) -> np.ndarray:
 
         gram_matrices = [b.gram_matrix() for b in self.basis_list]
 
         return scipy.linalg.block_diag(*gram_matrices)
 
-    def _coordinate_nonfull(self, fdatabasis, key):
+    def _coordinate_nonfull(
+        self,
+        fdatabasis: FDataBasis,
+        key: Union[int, range],
+    ) -> FDataBasis:
 
         r_key = key
         if isinstance(r_key, int):
             r_key = range(r_key, r_key + 1)
-            s_key = slice(r_key.start, r_key.stop, r_key.step)
+
+        s_key = slice(r_key.start, r_key.stop, r_key.step)
 
         coef_indexes = np.concatenate([
             np.ones(b.n_basis, dtype=np.bool_) if i in r_key
             else np.zeros(b.n_basis, dtype=np.bool_)
-            for i, b in enumerate(self.basis_list)])
+            for i, b in enumerate(self.basis_list)
+        ])
 
-        new_basis_list = self.basis_list[key]
+        new_basis_list = self.basis_list[s_key]
 
-        basis = (new_basis_list if isinstance(new_basis_list, Basis)
-                 else VectorValued(new_basis_list))
+        basis = (
+            new_basis_list[0] if isinstance(key, int)
+            else VectorValued(new_basis_list)
+        )
 
         coefs = fdatabasis.coefficients[:, coef_indexes]
 
         coordinate_names = np.array(fdatabasis.coordinate_names)[s_key]
 
-        return fdatabasis.copy(basis=basis, coefficients=coefs,
-                               coordinate_names=coordinate_names)
+        return fdatabasis.copy(
+            basis=basis,
+            coefficients=coefs,
+            coordinate_names=coordinate_names,
+        )
 
-    def __eq__(self, other):
+    def __repr__(self) -> str:
+        """Representation of a Basis object."""
+        return (
+            f"{self.__class__.__name__}("
+            f"basis_list={self.basis_list})"
+        )
+
+    def __eq__(self, other: Any) -> bool:
         return super().__eq__(other) and self.basis_list == other.basis_list
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash((super().__hash__(), self.basis_list))

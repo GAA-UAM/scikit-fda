0790cfdb6902fc7e22a06f798691bcbbd6c35ff8
mellamansanchez <alvarosanchezsr@gmail.com>
2021-02-15 20:31:43 +0100

    phaseplane

--- skfda/exploratory/visualization/_phase_plane_plot.py
+++ skfda/exploratory/visualization/_phase_plane_plot.py
@@ -2,14 +2,13 @@ from typing import List, Optional, TypeVar
 
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
+from ...representation import FData
 
 from ._utils import (
     _get_figure_and_axes,
-    _set_figure_layout,
     _set_figure_layout_for_fdata,
 )
 
-T = TypeVar('T')
 S = TypeVar('S', Figure, Axes, List[Axes])
 
 
@@ -17,8 +16,8 @@ class PhasePlanePlot:
 
     def __init__(
         self,
-        fdata1: T,
-        fdata2: Optional[T] = None,
+        fdata1: FData,
+        fdata2: Optional[FData] = None,
     ) -> None:
         self.fdata1 = fdata1
         self.fdata2 = fdata2
@@ -29,44 +28,39 @@ class PhasePlanePlot:
         *,
         fig: Optional[Figure] = None,
         axes: Optional[List[Axes]] = None,
-        n_rows: Optional[int] = None,
-        n_cols: Optional[int] = None,
         **kwargs,
     ) -> Figure:
         fig, axes = _get_figure_and_axes(chart, fig, axes)
 
         if (
-            self.fdata1.dim_domain == 1
-            and self.fdata1.dim_codomain == 2
-            and self.fdata2 is None
+            self.fdata2 is not None
         ):
-            fig, axes = _set_figure_layout(
-                fig,
-                axes,
-                dim=self.fdata1.dim_domain + 1,
-                n_axes=1,
-            )
-            axes[0].plot(
-                self.fdata1.data_matrix[0][0].tolist(),
-                self.fdata1.data_matrix[0][1].tolist(),
-                **kwargs,
-            )
+            if (
+                self.fdata1.dim_domain == self.fdata2.dim_domain
+                and self.fdata1.dim_codomain == self.fdata2.dim_codomain
+                and self.fdata1.dim_domain == 1
+                and self.fdata1.dim_codomain == 1
+            ):
+                fd = self.fdata1.concatenate()
+            else:
+                raise ValueError(
+                    "Error in data arguments",
+                )
+        else:
+            fd = self.fdata1
 
-        elif (
-            self.fdata1.dim_domain == self.fdata2.dim_domain
-            and self.fdata1.dim_codomain == self.fdata2.dim_codomain
-            and self.fdata1.dim_domain == 1
-            and self.fdata1.dim_codomain == 1
+        if (
+            fd.dim_domain == 1
+            and fd.dim_codomain == 2
         ):
             fig, axes = _set_figure_layout_for_fdata(
-                self.fdata1, fig, axes,
+                fd, fig, axes,
             )
             axes[0].plot(
-                self.fdata1.data_matrix[0].tolist(),
-                self.fdata2.data_matrix[0].tolist(),
+                fd.data_matrix[0][0].tolist(),
+                fd.data_matrix[0][1].tolist(),
                 **kwargs,
             )
-
         else:
             raise ValueError(
                 "Error in data arguments",

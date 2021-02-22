5a92721d799a3709c9aff0f64ba67986c7fc534c
mellamansanchez <alvarosanchezsr@gmail.com>
2021-02-22 15:12:16 +0100

    phase_plane

--- skfda/exploratory/visualization/_phase_plane_plot.py
+++ skfda/exploratory/visualization/_phase_plane_plot.py
@@ -1,3 +1,11 @@
+"""Phase-Plane Plot Module.
+
+This module contains the functionality in charge of plotting
+two different functions as coordinates, this can be done giving
+one FData, with domain 1 and codomain 2, or giving two FData, both
+of them with domain 1 and codomain 1.
+"""
+
 from typing import List, Optional, TypeVar
 
 from matplotlib.axes import Axes
@@ -13,7 +21,18 @@ S = TypeVar('S', Figure, Axes, List[Axes])
 
 
 class PhasePlanePlot:
+    """Phase-Plane Plot visualization.
 
+    This class contains the functionality in charge of plotting
+    two different functions as coordinates, this can be done giving
+    one FData, with domain 1 and codomain 2, or giving two FData, both
+    of them with domain 1 and codomain 1.
+    Args:
+        fdata1: functional data set that we will use for the graph. If it has
+            a dim_codomain = 1, the fdata2 will be needed.
+        fdata2: optional functional data set, that will be needed if the fdata1
+            has dim_codomain = 1.
+    """
     def __init__(
         self,
         fdata1: FData,
@@ -27,10 +46,28 @@ class PhasePlanePlot:
         chart: Optional[S] = None,
         *,
         fig: Optional[Figure] = None,
-        axes: Optional[List[Axes]] = None,
+        ax: Optional[Axes] = None,
         **kwargs,
     ) -> Figure:
-        fig, axes = _get_figure_and_axes(chart, fig, axes)
+        """
+        Plot Phase-Plane graph.
+
+        Plot the functions as coordinates. If two functions are passed
+        it will concatenate both into one only FData.
+        Args:
+            chart: figure over with the graphs are plotted or axis over
+                where the graphs are plotted. If None and ax is also
+                None, the figure is initialized.
+            fig: figure over with the graphs are plotted in case ax is not
+                specified. If None and ax is also None, the figure is
+                initialized.
+            ax: axis where the graphs are plotted. If None, see param fig.
+            kwargs: optional arguments.
+        Returns:
+            fig (figure object): figure object in which the phase-plane
+            graph will be plotted.
+        """
+        fig, axes = _get_figure_and_axes(chart, fig, ax)
 
         if (
             self.fdata2 is not None
@@ -46,7 +83,8 @@ class PhasePlanePlot:
                 )
             else:
                 raise ValueError(
-                    "Error in data arguments",
+                    "Error in data arguments,",
+                    "codomain or domain is not correct.",
                 )
         else:
             self.fd_final = self.fdata1
@@ -59,13 +97,14 @@ class PhasePlanePlot:
                 fig, axes, dim=2, n_axes=1,
             )
             axes[0].plot(
-                self.fd_final.data_matrix[0][:,0].tolist(),
-                self.fd_final.data_matrix[0][:,1].tolist(),
+                self.fd_final.data_matrix[0][:, 0].tolist(),
+                self.fd_final.data_matrix[0][:, 1].tolist(),
                 **kwargs,
             )
         else:
             raise ValueError(
-                "Error in data arguments",
+                "Error in data arguments,",
+                "codomain or domain is not correct.",
             )
 
         fig.suptitle("Phase-Plane Plot")

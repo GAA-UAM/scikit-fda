9c88e25327a488a1369a746e7de2ccdf93a92912
mellamansanchez <alvarosanchezsr@gmail.com>
2021-04-14 16:31:53 +0200

    baseplot ended all commented

--- skfda/exploratory/visualization/_baseplot.py
+++ skfda/exploratory/visualization/_baseplot.py
@@ -1,5 +1,12 @@
+"""BasePlot Module.
+
+This module contains the abstract class of which inherit all
+the visualization modules, containing the basic functionality
+common to all of them.
+"""
+
 from abc import ABC, abstractmethod
-from typing import List, Optional, Sequence, TypeVar, Union
+from typing import Optional, Sequence, Union
 
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
@@ -7,24 +14,43 @@ from matplotlib.figure import Figure
 
 from ._utils import _figure_to_svg
 
-S = TypeVar('S', Figure, Axes, List[Axes])
-
 
 class BasePlot(ABC):
+    """
+    BasePlot class.
+
+    Attributes:
+        id_function: list of PathCollection objects corresponding
+            to every instance of our plot. They will be used to modify
+            the visualization with interactivity and widgets.
+        fig: figure over with the graph is plotted.
+        axes: axis where the graph is plotted.
+    """
+
     @abstractmethod
     def __init__(
         self,
     ) -> None:
         self.id_function = []
+        self.axes = None
+        self.fig = None
 
     @abstractmethod
     def plot(
         self,
     ) -> Figure:
+        """
+        Abstract method used to plot the object and its data.
+
+        Returns:
+            Figure: figure object in which the displays and
+                widgets will be plotted.
+        """
         pass
 
     @abstractmethod
     def num_instances(self) -> int:
+        """Get the number of instances that will be used for interactivity."""
         pass
 
     @abstractmethod
@@ -34,15 +60,39 @@ class BasePlot(ABC):
         fig: Optional[Figure] = None,
         axes: Union[Axes, Sequence[Axes], None] = None,
     ) -> None:
+        """
+        Initialize the axes and fig of the plot.
+
+        Args:
+            chart: figure over with the graphs are plotted or axis over
+                where the graphs are plotted. If None and ax is also
+                None, the figure is initialized.
+            fig: figure over with the graphs are plotted in case ax is not
+                specified. If None and ax is also None, the figure is
+                initialized.
+            axes: axis where the graphs are plotted. If None, see param fig.
+        """
         pass
 
     def clear_ax(self) -> None:
+        """
+        Resets the basic attributes of the BasePlot.
+
+        Clear the old axes of the BasePlot and reset the
+        id_function list.
+        """
         for ax in self.axes:
             ax.clear()
         if len(self.id_function) != 0:
             self.id_function = []
 
-    def _repr_svg_(self):
+    def _repr_svg_(self) -> str:
+        """
+        Automatically represents the object as an svg when calling it.
+
+        Returns:
+            str: string containing the xml code used to get the svg.
+        """
         self.fig = self.plot()
         plt.close(self.fig)
         return _figure_to_svg(self.fig)

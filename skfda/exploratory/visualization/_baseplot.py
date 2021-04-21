b1d06b40dec6676122ff2cffe7d285f4085c0981
mellamansanchez <alvarosanchezsr@gmail.com>
2021-04-21 16:07:35 +0200

    deleted method setfigureandaxes

--- skfda/exploratory/visualization/_baseplot.py
+++ skfda/exploratory/visualization/_baseplot.py
@@ -56,27 +56,6 @@ class BasePlot(ABC):
         """Get the number of instances that will be used for interactivity."""
         pass
 
-    @abstractmethod
-    def set_figure_and_axes(
-        self,
-        chart: Union[Figure, Axes, None] = None,
-        fig: Optional[Figure] = None,
-        axes: Union[Axes, Sequence[Axes], None] = None,
-    ) -> None:
-        """
-        Initialize the axes and fig of the plot.
-
-        Args:
-            chart: figure over with the graphs are plotted or axis over
-                where the graphs are plotted. If None and ax is also
-                None, the figure is initialized.
-            fig: figure over with the graphs are plotted in case ax is not
-                specified. If None and ax is also None, the figure is
-                initialized.
-            axes: axis where the graphs are plotted. If None, see param fig.
-        """
-        pass
-
     def _repr_svg_(self) -> str:
         """Automatically represents the object as an svg when calling it."""
         self.fig = self.plot()

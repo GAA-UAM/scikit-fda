032659f2ec29b40ef4eb13f8f07f653845add678
mellamansanchez <alvarosanchezsr@gmail.com>
2021-04-22 12:21:55 +0200

    multiple display ended with all functions

--- skfda/exploratory/visualization/__init__.py
+++ skfda/exploratory/visualization/__init__.py
@@ -5,6 +5,7 @@ from ._baseplot import BasePlot
 from ._boxplot import Boxplot, SurfaceBoxplot
 from ._ddplot import DDPlot
 from ._magnitude_shape_plot import MagnitudeShapePlot
+from ._multiple_display import MultipleDisplay
 from ._outliergram import Outliergram
 from ._parametric_plot import ParametricPlot
 from .fpca import plot_fpca_perturbation_graphs

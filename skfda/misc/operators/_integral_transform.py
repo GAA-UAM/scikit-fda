3390894c7b0dbcb68189908795a835d7a64632ce
VNMabus <vnmabus@gmail.com>
2021-05-08 14:51:53 +0200

    Improve operators.
    Add typing and style.
    Get rid or _FDataCallable.
    Allow callables as weights for LinearDifferentialOperator, and simplify
    its logic.

--- skfda/misc/operators/_integral_transform.py
+++ skfda/misc/operators/_integral_transform.py
@@ -1,33 +1,52 @@
+from __future__ import annotations
+
+from typing import Callable
+
+import numpy as np
+
 import scipy.integrate
 
+from ...representation import FData
 from ._operators import Operator
 
 
-class IntegralTransform(Operator):
+class IntegralTransform(Operator[FData, Callable[[np.ndarray], np.ndarray]]):
     """Integral operator.
 
-
-
-    Attributes:
-        kernel_function (callable):  Kernel function corresponding to
-                        the operator.
+    Parameters:
+        kernel_function:  Kernel function corresponding to the operator.
 
     """
 
-    def __init__(self, kernel_function):
+    def __init__(
+        self,
+        kernel_function: Callable[[np.ndarray, np.ndarray], np.ndarray],
+    ) -> None:
         self.kernel_function = kernel_function
 
-    def __call__(self, f):
+    def __call__(  # noqa: D102
+        self,
+        f: FData,
+    ) -> Callable[[np.ndarray], np.ndarray]:
 
-        def evaluate_covariance(points):
+        def evaluate_covariance(  # noqa: WPS430
+            points: np.ndarray,
+        ) -> np.ndarray:
 
-            def integral_body(integration_var):
-                return (f(integration_var) *
-                        self.kernel_function(integration_var, points))
+            def integral_body(  # noqa: WPS430
+                integration_var: np.ndarray,
+            ) -> np.ndarray:
+                return (
+                    f(integration_var)
+                    * self.kernel_function(integration_var, points)
+                )
 
             domain_range = f.domain_range[0]
 
             return scipy.integrate.quad_vec(
-                integral_body, domain_range[0], domain_range[1])[0]
+                integral_body,
+                domain_range[0],
+                domain_range[1],
+            )[0]
 
         return evaluate_covariance
--- skfda/misc/operators/_operators.py
+++ skfda/misc/operators/_operators.py
@@ -1,22 +1,43 @@
+from __future__ import annotations
+
 import abc
+from typing import Callable, Generic, TypeVar, Union
 
 import multimethod
+import numpy as np
+
+from ...representation import FData
+
+OperatorInput = TypeVar(
+    "OperatorInput",
+    bound=Union[np.ndarray, FData],
+    contravariant=True,
+)
+
+OperatorOutput = TypeVar(
+    "OperatorOutput",
+    bound=Union[np.ndarray, Callable[[np.ndarray], np.ndarray]],
+    covariant=True,
+)
 
 
-class Operator(abc.ABC):
-    """
-    Abstract class for :term:`operators`.
-
-    """
+class Operator(abc.ABC, Generic[OperatorInput, OperatorOutput]):
+    """Abstract class for :term:`operators`."""
 
     @abc.abstractmethod
-    def __call__(self, vector):
+    def __call__(self, vector: OperatorInput) -> OperatorOutput:
+        """Evaluate the operator."""
         pass
 
 
 @multimethod.multidispatch
-def gramian_matrix_optimization(linear_operator, basis):
-    r"""
+def gramian_matrix_optimization(
+    linear_operator: Callable[[OperatorInput], OperatorOutput],
+    basis: OperatorInput,
+) -> np.ndarray:
+    """
+    Efficient implementation of gramian_matrix.
+
     Generic function that can be subclassed for different combinations of
     operator and basis in order to provide a more efficient implementation
     for the gramian matrix.
@@ -24,8 +45,11 @@ def gramian_matrix_optimization(linear_operator, basis):
     return NotImplemented
 
 
-def gramian_matrix_numerical(linear_operator, basis):
-    r"""
+def gramian_matrix_numerical(
+    linear_operator: Callable[[OperatorInput], OperatorOutput],
+    basis: OperatorInput,
+) -> np.ndarray:
+    """
     Return the gramian matrix given a basis, computed numerically.
 
     This method should work for every linear operator.
@@ -35,10 +59,15 @@ def gramian_matrix_numerical(linear_operator, basis):
 
     evaluated_basis = linear_operator(basis)
 
-    return inner_product_matrix(evaluated_basis)
+    domain_range = getattr(basis, "domain_range", None)
+
+    return inner_product_matrix(evaluated_basis, _domain_range=domain_range)
 
 
-def gramian_matrix(linear_operator, basis):
+def gramian_matrix(
+    linear_operator: Callable[[OperatorInput], OperatorOutput],
+    basis: OperatorInput,
+) -> np.ndarray:
     r"""
     Return the gramian matrix given a basis.
 
@@ -55,7 +84,6 @@ def gramian_matrix(linear_operator, basis):
     falling back to a numerical computation otherwise.
 
     """
-
     # Try to use a more efficient implementation
     matrix = gramian_matrix_optimization(linear_operator, basis)
     if matrix is not NotImplemented:
@@ -64,20 +92,19 @@ def gramian_matrix(linear_operator, basis):
     return gramian_matrix_numerical(linear_operator, basis)
 
 
-class MatrixOperator(Operator):
+class MatrixOperator(Operator[np.ndarray, np.ndarray]):
     """Linear operator for finite spaces.
 
     Between finite dimensional spaces, every linear operator can be expressed
     as a product by a matrix.
 
     Attributes:
-        matrix (array-like object):  The matrix containing the linear
-               transformation.
+        matrix:  The matrix containing the linear transformation.
 
     """
 
-    def __init__(self, matrix):
+    def __init__(self, matrix: np.ndarray) -> None:
         self.matrix = matrix
 
-    def __call__(self, f):
+    def __call__(self, f: np.ndarray) -> np.ndarray:  # noqa: D102
         return self.matrix @ f

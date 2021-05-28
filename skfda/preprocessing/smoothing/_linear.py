41adc5c7ba463278c790fa0a9da32b4df8116b18
VNMabus <vnmabus@gmail.com>
2021-05-29 01:53:11 +0200

    Simplify linear smoothers logic.

--- skfda/preprocessing/smoothing/_linear.py
+++ skfda/preprocessing/smoothing/_linear.py
@@ -4,13 +4,17 @@
 This module contains the abstract base class for all linear smoothers.
 
 """
+from __future__ import annotations
+
 import abc
+from typing import Any, Mapping, Optional
 
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 
 from ... import FDataGrid
 from ..._utils import _to_grid_points
+from ...representation._typing import GridPointsLike
 
 
 class _LinearSmoother(
@@ -28,47 +32,45 @@ class _LinearSmoother(
     def __init__(
         self,
         *,
-        output_points=None,
+        output_points: Optional[GridPointsLike] = None,
     ):
         self.output_points = output_points
 
-    def hat_matrix(self, input_points=None, output_points=None):
-        cached_input_points = getattr(self, "input_points_", None)
-        cached_output_points = getattr(self, "output_points_", None)
+    def hat_matrix(
+        self,
+        input_points: Optional[GridPointsLike] = None,
+        output_points: Optional[GridPointsLike] = None,
+    ) -> np.ndarray:
 
         # Use the fitted points if they are not provided
         if input_points is None:
-            input_points = cached_input_points
+            input_points = self.input_points_
         if output_points is None:
-            output_points = cached_output_points
+            output_points = self.output_points_
 
-        if (cached_input_points is not None and
-            np.array_equal(input_points, cached_input_points) and
-                np.array_equal(output_points, cached_output_points)):
-            cached_hat_matrix = getattr(self, "_cached_hat_matrix", None)
-            if cached_hat_matrix is None:
-                self.cached_hat_matrix = self._hat_matrix(
-                    input_points=self.input_points_,
-                    output_points=self.output_points_,
-                )
-            return self.cached_hat_matrix
-
-        # We only cache the matrix for the fit points
         return self._hat_matrix(
             input_points=self.input_points_,
             output_points=self.output_points_,
         )
 
     @abc.abstractmethod
-    def _hat_matrix(self, input_points, output_points):
+    def _hat_matrix(
+        self,
+        input_points: GridPointsLike,
+        output_points: GridPointsLike,
+    ) -> np.ndarray:
         pass
 
-    def _more_tags(self):
+    def _more_tags(self) -> Mapping[str, Any]:
         return {
-            'X_types': []
+            'X_types': [],
         }
 
-    def fit(self, X: FDataGrid, y=None):
+    def fit(
+        self,
+        X: FDataGrid,
+        y: None = None,
+    ) -> _LinearSmoother:
         """Compute the hat matrix for the desired output points.
 
         Args:
@@ -87,23 +89,26 @@ class _LinearSmoother(
             else self.input_points_
         )
 
-        # Force caching the hat matrix
-        self.hat_matrix()
+        self.hat_matrix_ = self.hat_matrix()
 
         return self
 
-    def transform(self, X: FDataGrid, y=None):
-        """Multiplies the hat matrix for the functions values to smooth them.
+    def transform(
+        self,
+        X: FDataGrid,
+        y: None = None,
+    ) -> FDataGrid:
+        """Multiply the hat matrix with the function values to smooth them.
 
         Args:
             X (FDataGrid):
                 The data to smooth.
             y : Ignored
+
         Returns:
             FDataGrid: Functional data smoothed.
 
         """
-
         assert all(
             np.array_equal(i, s) for i, s in zip(
                 self.input_points_,
@@ -117,14 +122,19 @@ class _LinearSmoother(
             grid_points=self.output_points_,
         )
 
-    def score(self, X, y):
-        """Returns the generalized cross validation (GCV) score.
+    def score(
+        self,
+        X: FDataGrid,
+        y: FDataGrid,
+    ) -> float:
+        """Return the generalized cross validation (GCV) score.
 
         Args:
             X (FDataGrid):
                 The data to smooth.
             y (FDataGrid):
                 The target data. Typically the same as ``X``.
+
         Returns:
             float: Generalized cross validation score.
 

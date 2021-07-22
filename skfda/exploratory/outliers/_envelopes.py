972c6622e403a720f30fbddf8be0faaf8a747035
VNMabus <vnmabus@gmail.com>
2021-07-22 13:06:52 +0200

    Typing IQROutlierDetector

--- skfda/exploratory/outliers/_envelopes.py
+++ skfda/exploratory/outliers/_envelopes.py
@@ -1,24 +1,35 @@
+from __future__ import annotations
+
 import math
+from typing import Tuple
 
 import numpy as np
 
+from ...representation import FDataGrid
 
-def _compute_region(fdatagrid,
-                    indices_descending_depth,
-                    prob):
+
+def _compute_region(
+    fdatagrid: FDataGrid,
+    indices_descending_depth: np.ndarray,
+    prob: float,
+) -> FDataGrid:
     indices_samples = indices_descending_depth[
-        :math.ceil(fdatagrid.n_samples * prob)]
+        :math.ceil(fdatagrid.n_samples * prob)
+    ]
     return fdatagrid[indices_samples]
 
 
-def _compute_envelope(region):
+def _compute_envelope(region: FDataGrid) -> Tuple[np.ndarray, np.ndarray]:
     max_envelope = np.max(region.data_matrix, axis=0)
     min_envelope = np.min(region.data_matrix, axis=0)
 
     return min_envelope, max_envelope
 
 
-def _predict_outliers(fdatagrid, non_outlying_threshold):
+def _predict_outliers(
+    fdatagrid: FDataGrid,
+    non_outlying_threshold: Tuple[np.ndarray, np.ndarray],
+) -> np.ndarray:
     # A functional datum is considered an outlier if it has ANY point
     # in ANY dimension outside the envelope for inliers
 
@@ -26,19 +37,26 @@ def _predict_outliers(fdatagrid, non_outlying_threshold):
 
     or_axes = tuple(i for i in range(1, fdatagrid.data_matrix.ndim))
 
-    below_outliers = np.any(fdatagrid.data_matrix <
-                            min_threshold, axis=or_axes)
-    above_outliers = np.any(fdatagrid.data_matrix >
-                            max_threshold, axis=or_axes)
+    below_outliers = np.any(
+        fdatagrid.data_matrix < min_threshold,
+        axis=or_axes,
+    )
+    above_outliers = np.any(
+        fdatagrid.data_matrix > max_threshold,
+        axis=or_axes,
+    )
 
     return below_outliers | above_outliers
 
 
-def _non_outlying_threshold(central_envelope, factor):
+def _non_outlying_threshold(
+    central_envelope: Tuple[np.ndarray, np.ndarray],
+    factor: float,
+) -> Tuple[np.ndarray, np.ndarray]:
     iqr = central_envelope[1] - central_envelope[0]
     non_outlying_threshold_max = central_envelope[1] + iqr * factor
     non_outlying_threshold_min = central_envelope[0] - iqr * factor
-    non_outlying_threshold = (non_outlying_threshold_min,
-                              non_outlying_threshold_max)
-
-    return non_outlying_threshold
+    return (
+        non_outlying_threshold_min,
+        non_outlying_threshold_max,
+    )

a9953a3104195ce9796397d094b17b1b90fd090f
VNMabus <vnmabus@gmail.com>
2021-10-25 02:13:22 +0200

    Fix examples.

--- examples/plot_magnitude_shape_synthetic.py
+++ examples/plot_magnitude_shape_synthetic.py
@@ -12,9 +12,9 @@ Shows the use of the MS-Plot applied to a synthetic dataset.
 
 import matplotlib.pyplot as plt
 import numpy as np
+
 import skfda
 from skfda.exploratory.visualization import MagnitudeShapePlot
-
 
 ##############################################################################
 # First, we generate a synthetic dataset following [DaWe18]_
@@ -23,68 +23,89 @@ random_state = np.random.RandomState(0)
 n_samples = 200
 
 fd = skfda.datasets.make_gaussian_process(
-    n_samples=n_samples, n_features=100,
+    n_samples=n_samples,
+    n_features=100,
     cov=skfda.misc.covariances.Exponential(),
     mean=lambda t: 4 * t,
-    random_state=random_state)
+    random_state=random_state,
+)
 
 ##############################################################################
 # We now add the outliers
 
 magnitude_outlier = skfda.datasets.make_gaussian_process(
-    n_samples=1, n_features=100,
+    n_samples=1,
+    n_features=100,
     cov=skfda.misc.covariances.Exponential(),
     mean=lambda t: 4 * t + 20,
-    random_state=random_state)
+    random_state=random_state,
+)
 
 shape_outlier_shift = skfda.datasets.make_gaussian_process(
-    n_samples=1, n_features=100,
+    n_samples=1,
+    n_features=100,
     cov=skfda.misc.covariances.Exponential(),
     mean=lambda t: 4 * t + 10 * (t > 0.4),
-    random_state=random_state)
+    random_state=random_state,
+)
 
 shape_outlier_peak = skfda.datasets.make_gaussian_process(
-    n_samples=1, n_features=100,
+    n_samples=1,
+    n_features=100,
     cov=skfda.misc.covariances.Exponential(),
     mean=lambda t: 4 * t - 10 * ((0.25 < t) & (t < 0.3)),
-    random_state=random_state)
+    random_state=random_state,
+)
 
 shape_outlier_sin = skfda.datasets.make_gaussian_process(
-    n_samples=1, n_features=100,
+    n_samples=1,
+    n_features=100,
     cov=skfda.misc.covariances.Exponential(),
     mean=lambda t: 4 * t + 2 * np.sin(18 * t),
-    random_state=random_state)
+    random_state=random_state,
+)
 
 shape_outlier_slope = skfda.datasets.make_gaussian_process(
-    n_samples=1, n_features=100,
+    n_samples=1,
+    n_features=100,
     cov=skfda.misc.covariances.Exponential(),
     mean=lambda t: 10 * t,
-    random_state=random_state)
+    random_state=random_state,
+)
 
 magnitude_shape_outlier = skfda.datasets.make_gaussian_process(
-    n_samples=1, n_features=100,
+    n_samples=1,
+    n_features=100,
     cov=skfda.misc.covariances.Exponential(),
     mean=lambda t: 4 * t + 2 * np.sin(18 * t) - 20,
-    random_state=random_state)
+    random_state=random_state,
+)
 
 
-fd = fd.concatenate(magnitude_outlier, shape_outlier_shift,
-                    shape_outlier_peak, shape_outlier_sin,
-                    shape_outlier_slope, magnitude_shape_outlier)
+fd = fd.concatenate(
+    magnitude_outlier,
+    shape_outlier_shift,
+    shape_outlier_peak,
+    shape_outlier_sin,
+    shape_outlier_slope,
+    magnitude_shape_outlier,
+)
 
 ##############################################################################
 # The data is plotted to show the curves we are working with.
 labels = [0] * n_samples + [1] * 6
 
-fd.plot(group=labels,
-        group_colors=['lightgrey', 'black'])
+fd.plot(
+    group=labels,
+    group_colors=['lightgrey', 'black'],
+)
 
 ##############################################################################
 # The MS-Plot is generated. In order to show the results, the
 # :func:`~skfda.exploratory.visualization.MagnitudeShapePlot.plot`
 # method is used.
 
-msplot = MagnitudeShapePlot(fdatagrid=fd)
+msplot = MagnitudeShapePlot(fd)
 
 msplot.plot()
 
@@ -93,19 +114,31 @@ msplot.plot()
 # in a different color
 
 labels = [0] * n_samples + [1, 2, 3, 4, 5, 6]
-colors = ['lightgrey', 'orange', 'blue', 'black',
-          'green', 'brown', 'lightblue']
+colors = [
+    'lightgrey',
+    'orange',
+    'blue',
+    'black',
+    'green',
+    'brown',
+    'lightblue',
+]
 
-fd.plot(group=labels,
-        group_colors=colors)
+fd.plot(
+    group=labels,
+    group_colors=colors,
+)
 
 ##############################################################################
 # We now show the points in the MS-plot using the same colors
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-ax.scatter(msplot.points[:, 0].ravel(), msplot.points[:, 1].ravel(),
-           c=colors[0:1] * n_samples + colors[1:])
+ax.scatter(
+    msplot.points[:, 0].ravel(),
+    msplot.points[:, 1].ravel(),
+    c=colors[:1] * n_samples + colors[1:],
+)
 ax.set_title("MS-Plot")
 ax.set_xlabel("magnitude outlyingness")
 ax.set_ylabel("shape outlyingness")

3cc9ed54b04f22391f9b0fe36f6cea1fefe12bf7
Alvaro <lalocgarcia@gmail.com>
2021-11-03 19:46:27 +0100

    Fix branch with issue 377

--- skfda/preprocessing/dim_reduction/feature_extraction/__init__.py
+++ skfda/preprocessing/dim_reduction/feature_extraction/__init__.py
@@ -2,4 +2,3 @@
 from ._ddg_transformer import DDGTransformer
 from ._fda_feature_union import FdaFeatureUnion
 from ._fpca import FPCA
-from ._per_class_feature_transformer import PerClassFeatureTransformer

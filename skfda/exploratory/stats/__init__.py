9f1f5839773eeb9e9935365bac7178243efd64ed
Alvaro <lalocgarcia@gmail.com>
2022-02-03 23:27:59 +0100

    Occupation measure construction

--- skfda/exploratory/stats/__init__.py
+++ skfda/exploratory/stats/__init__.py
@@ -1,9 +1,5 @@
 from ._fisher_rao import _fisher_rao_warping_mean, fisher_rao_karcher_mean
-from ._functional_transformers import (
-    local_averages,
-    number_up_crossings,
-    occupation_measure,
-)
+from ._functional_transformers import occupation_measure
 from ._stats import (
     cov,
     depth_based_median,

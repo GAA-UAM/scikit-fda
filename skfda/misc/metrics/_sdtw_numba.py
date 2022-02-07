37b5b16aff1721cfd0c301192d717589d4814c0b
Lejeune <clementlej@gmail.com>
2022-02-07 12:33:56 +0100

    conversion of sdtw in cython to python@numba

---
+++ skfda/misc/metrics/_sdtw_numba.py
@@ -0,0 +1,34 @@
+import numpy as np
+from numba import njit
+
+
+@njit(cache=True, fastmath=True)
+def _soft_min_argmin(x, y, z):
+    """soft-min of three real numbers"""
+
+    min_xyz = np.minimum(x, np.minimum(y, z))
+    nn = np.exp(min_xyz - x)
+    nn += np.exp(min_xyz - y)
+    nn += np.exp(min_xyz - z)
+
+    return min_xyz - np.log(nn)
+
+
+@njit(cache=True, fastmath=True)
+def _sdtw(cost_mat, gamma=1.0):
+    """soft-dtw divergence with dynamic recursion"""
+
+    len_x = cost_mat.shape[0]
+    len_y = cost_mat.shape[1]
+
+    V = np.zeros(shape=(len_x + 1, len_y + 1))
+    V[0, 1:] = np.inf
+    V[1:, 0] = np.inf
+
+    for i in range(1, len_x + 1):
+        for j in range(1, len_y + 1):
+
+            V[i, j] = (cost_mat[i - 1, j - 1] / gamma) \
+                + _soft_min_argmin(V[i, j - 1], V[i - 1, j - 1], V[i - 1, j])
+
+    return gamma * V[len_x, len_y]

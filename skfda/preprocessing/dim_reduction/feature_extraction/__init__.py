275d3342feced7ce1e6cb553f6cc74ba0ae59ad6
Alvaro <lalocgarcia@gmail.com>
2022-04-20 00:53:37 +0200

    Init corrections

--- skfda/preprocessing/dim_reduction/feature_extraction/__init__.py
+++ skfda/preprocessing/dim_reduction/feature_extraction/__init__.py
@@ -1,9 +1,10 @@
 import warnings
 
-from ... import feature_construction
+from .. import FPCA
 
 warnings.warn(
     'The module "feature_extraction" is deprecated.'
-    'Please use "feature_construction"',
+    'Please use "dim_reduction" for FPCA'
+    'or "feature_construction" for feature construction techniques',
     category=DeprecationWarning,
 )

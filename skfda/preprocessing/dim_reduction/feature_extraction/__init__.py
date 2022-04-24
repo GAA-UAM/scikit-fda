4526e50b61b6d75fcce3a150177401e3f9f6432f
Alvaro <lalocgarcia@gmail.com>
2022-04-25 00:02:54 +0200

    Function transformers

--- skfda/preprocessing/dim_reduction/feature_extraction/__init__.py
+++ skfda/preprocessing/dim_reduction/feature_extraction/__init__.py
@@ -2,4 +2,9 @@
 from ._ddg_transformer import DDGTransformer
 from ._fda_feature_union import FDAFeatureUnion
 from ._fpca import FPCA
+from ._function_transformers import (
+    LocalAveragesTransformer,
+    NumberUpCrossingsTransformer,
+    OccupationMeasureTransformer,
+)
 from ._per_class_transformer import PerClassTransformer

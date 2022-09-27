1e51985c1c22ad9dafd425649e6faa4f273901af
pedrog99 <pedrog99psj@gmail.com>
2022-09-27 18:53:06 +0200

    Trying to solve pull requests error v2

--- conftest.py
+++ conftest.py
@@ -11,14 +11,14 @@ except TypeError:
     pass
 
 """
-I introduced this change in order to adapt it to Windows 10 
+I introduced this change in order to adapt it to Windows 10
 operating system.
 More information about this problem in this GitHub issue:
 https://github.com/jupyter/jupyter-sphinx/issues/171#issuecomment-766953182
 """
-
 if (sys.version_info[0] == 3 and sys.version_info[1] >= 8
-        and sys.platform.startswith('win')):
+        and sys.platform.startswith('win')
+    ):
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 collect_ignore = ['setup.py', 'docs/conf.py']

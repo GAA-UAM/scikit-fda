68ad2660f35ffac10e778069d9cde462ff252975
Martín Sánchez Signorini <mssignorini@gmail.com>
2022-10-24 23:27:49 +0200

    ids for better visualization during collect

--- skfda/tests/test_classifier_classes.py
+++ skfda/tests/test_classifier_classes.py
@@ -15,18 +15,23 @@ from skfda.representation import FData
 from ..typing._numpy import NDArrayAny
 
 
-@pytest.fixture(params=[
-    LogisticRegression(),
-])
+@pytest.fixture(
+    params=[
+        LogisticRegression(),
+    ],
+    ids=lambda clf: type(clf).__name__,
+)
 def classifier(request: Any) -> Any:
     """Fixture for classifiers to test."""
     return request.param
 
 
-@pytest.fixture(params=[
-    np.array([0, 1]),
-    np.array(["class_a", "class_b"]),
-])
+@pytest.fixture(
+    params=[
+        np.array([0, 1]),
+        np.array(["class_a", "class_b"]),
+    ], ids=["int", "str"],
+)
 def classes(request: Any) -> Any:
     """Fixture for classes to test."""
     return request.param

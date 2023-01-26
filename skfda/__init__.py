c1f3a58b2f3e5ce8c76c13cf855f0cdbee41501d
VNMabus <vnmabus@gmail.com>
2023-01-26 16:45:59 +0100

    Move all config to pyproject.toml.

--- skfda/__init__.py
+++ skfda/__init__.py
@@ -30,18 +30,4 @@ if TYPE_CHECKING:
         concatenate as concatenate,
     )
 
-try:
-    with open(
-        _os.path.join(
-            _os.path.dirname(__file__),
-            '..',
-            'VERSION',
-        ),
-        'r',
-    ) as version_file:
-        __version__ = version_file.read().strip()
-except IOError as e:
-    if e.errno != _errno.ENOENT:
-        raise
-
-    __version__ = "0.0"
+__version__ = "0.8"

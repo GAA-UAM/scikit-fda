50714821e4608095dd1adeef71801303eaa06521
Ddelval <delvaldavid1@gmail.com>
2023-02-22 15:42:17 +0100

    Fix import

--- skfda/ml/regression/__init__.py
+++ skfda/ml/regression/__init__.py
@@ -3,14 +3,13 @@ from typing import TYPE_CHECKING
 
 import lazy_loader as lazy
 
-from ._fpca_regression import FPCARegression
-
 __getattr__, __dir__, __all__ = lazy.attach(
     __name__,
     submod_attrs={
         "_historical_linear_model": ["HistoricalLinearRegression"],
         "_kernel_regression": ["KernelRegression"],
         "_linear_regression": ["LinearRegression"],
+        "_fpca_regression": ["FPCARegression"],
         "_neighbors_regression": [
             "KNeighborsRegressor",
             "RadiusNeighborsRegressor",
@@ -19,6 +18,7 @@ __getattr__, __dir__, __all__ = lazy.attach(
 )
 
 if TYPE_CHECKING:
+    from ._fpca_regression import FPCARegression
     from ._historical_linear_model import (
         HistoricalLinearRegression as HistoricalLinearRegression,
     )

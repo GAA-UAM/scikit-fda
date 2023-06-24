b81b34b5e1bb69cc678b867374a1eb7188196704
David del Val <delvaldavid1@gmail.com>
2023-06-24 14:40:24 +0200

    Change to int  | None

--- skfda/representation/_functional_data.py
+++ skfda/representation/_functional_data.py
@@ -786,7 +786,7 @@ class FData(  # noqa: WPS214
     def sum(  # noqa: WPS125
         self: T,
         *,
-        axis: Optional[int] = None,
+        axis: int | None = None,
         out: None = None,
         keepdims: bool = False,
         skipna: bool = False,

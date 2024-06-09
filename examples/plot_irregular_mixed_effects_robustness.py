16c513af5a6927b91fa0015c39a9555d9917258d
Pablo Cuesta Sierra <pablocsaa@gmail.com>
2024-06-09 20:14:10 +0200

    decimation example

--- examples/plot_irregular_mixed_effects_robustness.py
+++ examples/plot_irregular_mixed_effects_robustness.py
@@ -64,37 +64,16 @@ train_original, test_original = train_test_split(
 # %%
 # Then, we create datasets with decreasing number of measurement points per
 # curve, by removing measurement points from the previous dataset iteratively.
-train_irregular_list = [train_original]
-test_irregular_list = [test_original]
 n_points_list = [365, 40, 10, 7, 5, 4, 3]
+train_irregular_datasets = {}
+test_irregular_datasets = {}
+current_train = train_original
+current_test = test_original
 for n_points in n_points_list:
-    train_irregular_list.append(
-        irregular_sample(
-            train_irregular_list[-1],
-            n_points_per_curve=n_points,
-            random_state=random_state,
-        ),
-    )
-    test_irregular_list.append(
-        irregular_sample(
-            test_irregular_list[-1],
-            n_points_per_curve=n_points,
-            random_state=random_state,
-        ),
-    )
-
-train_irregular_datasets = {
-    n_points: train_irregular
-    for n_points, train_irregular in zip(
-        n_points_list, train_irregular_list[1:],
-    )
-}
-test_irregular_datasets = {
-    n_points: test_irregular
-    for n_points, test_irregular in zip(
-        n_points_list, test_irregular_list[1:],
-    )
-}
+    current_train = irregular_sample(current_train, n_points, random_state)
+    current_test = irregular_sample(current_test, n_points, random_state)
+    train_irregular_datasets[n_points] = current_train
+    test_irregular_datasets[n_points] = current_test
 
 # %%
 # We convert the irregular data to basis representation and compute the scores.
@@ -102,21 +81,14 @@ test_irregular_datasets = {
 # the converter with a train set that has :math:`k` points per curve, we
 # use it to transform that train set, the test set with :math:`k` points per
 # curve and the original test set with 365 points per curve.
-score_functions = {
-    "R^2": r2_score,
-    "MSE": mean_squared_error,
-}
-converted_data = {
-    "Train-sparse": {},
-    "Test-sparse": {},
-    "Test-original": {},
-}
+score_functions = {"R^2": r2_score, "MSE": mean_squared_error}
+converted_data = {"Train-sparse": {}, "Test-sparse": {}, "Test-original": {}}
 scores = {
     score_name: {
         "n_points_per_curve": n_points_list,
-        **{data_name: [] for data_name in converted_data.keys()},
+        **{data_name: [] for data_name in converted_data},
     }
-    for score_name in score_functions.keys()
+    for score_name in score_functions
 }
 converter = EMMixedEffectsConverter(basis)
 for n_points, train_irregular, test_irregular in zip(
@@ -125,28 +97,23 @@ for n_points, train_irregular, test_irregular in zip(
     test_irregular_datasets.values(),
 ):
     converter = converter.fit(train_irregular)
-    train_sparse_converted = converter.transform(train_irregular)
-    test_sparse_converted = converter.transform(test_irregular)
-    test_original_converted = converter.transform(
-        FDataIrregular.from_fdatagrid(test_original),
-    )
-    converted_data["Train-sparse"][n_points] = train_sparse_converted
-    converted_data["Test-sparse"][n_points] = test_sparse_converted
-    converted_data["Test-original"][n_points] = test_original_converted
-
+    transformed = {
+        "Train-sparse": converter.transform(train_irregular),
+        "Test-sparse": converter.transform(test_irregular),
+        "Test-original": converter.transform(
+            FDataIrregular.from_fdatagrid(test_original),
+        ),
+    }
+    # Store the converted data
+    for key, data in transformed.items():
+        converted_data[key][n_points] = data
+    # Calculate and store the scores
     for score_name, score_fun in score_functions.items():
-        scores[score_name]["Train-sparse"].append(score_fun(
-            train_original,
-            train_sparse_converted.to_grid(train_original.grid_points),
-        ))
-        scores[score_name]["Test-sparse"].append(score_fun(
-            test_original,
-            test_sparse_converted.to_grid(test_original.grid_points),
-        ))
-        scores[score_name]["Test-original"].append(score_fun(
-            test_original,
-            test_original_converted.to_grid(test_original.grid_points),
-        ))
+        for key in converted_data:
+            scores[score_name][key].append(score_fun(
+                test_original if "Test" in key else train_original,
+                transformed[key].to_grid(test_original.grid_points),
+            ))
 
 # %%
 # Finally, we have the scores for the train and test sets with decreasing
@@ -250,11 +217,11 @@ def plot_conversion_evolution(index: int):
 
 
 # %%
-# Toronto station's temperature curve conversion evolution.
+# Toronto station's temperature curve conversion evolution:
 plot_conversion_evolution(index=7)
 
 # %%
-# Iqaluit station's temperature curve conversion evolution.
+# Iqaluit station's temperature curve conversion evolution:
 plot_conversion_evolution(index=8)
 
 # %%

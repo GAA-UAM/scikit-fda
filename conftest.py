da6d11e887267b4d9998351e859abd6b79affe91
Diego Rodríguez Ortiz <diego.rodriguezo@estudiante.uam.es>
2026-06-14 17:59:01 +0200

    Ruff changes for conftest to pass

--- conftest.py
+++ conftest.py
@@ -1,15 +1,16 @@
-import pytest
-import sys
+"""Configuration file for pytest."""
 import asyncio
+import contextlib
 import importlib.util
+import sys
+
 import numpy as np
+import pytest
 
 # https://github.com/scikit-learn/scikit-learn/issues/8959
 
-try:
-    np.set_printoptions(sign=' ')
-except TypeError:
-    pass
+with contextlib.suppress(TypeError):
+    np.set_printoptions(sign=" ")
 
 
 # I introduced this change in order to adapt it to Windows 10
@@ -18,12 +19,12 @@ except TypeError:
 # https://github.com/jupyter/jupyter-sphinx/issues/171#issuecomment-766953182
 
 if (
-    sys.version_info[0] == 3 and sys.version_info[1] >= 8
-        and sys.platform.startswith('win')
+    sys.version_info[0] >= 3 and sys.version_info[1] >= 8  # noqa: PLR2004, YTT203
+        and sys.platform.startswith("win")
 ):
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-collect_ignore = ['setup.py', 'docs/conf.py', 'asv_benchmarks']
+collect_ignore = ["setup.py", "docs/conf.py", "asv_benchmarks"]
 
 # PyTorch is an optional dependency, required only by skfda.ml.generative.
 # When it is absent, --doctest-modules cannot import the generative source

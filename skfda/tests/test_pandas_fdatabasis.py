c1c3a42f014da8c8666f681204afaa11a2b460b3
Carlos Ramos Carreño <vnmabus@gmail.com>
2026-08-27 17:28:44 +0200

    FDataBasis: Fix Pandas tests
    Pandas tests for FDataBasis should now pass with the new Pandas version.

--- skfda/tests/test_pandas_fdatabasis.py
+++ skfda/tests/test_pandas_fdatabasis.py
@@ -1,12 +1,14 @@
+"""Tests for Pandas ExtensionArray for FDataBasis."""
 from __future__ import annotations
 
-from typing import Any, Callable, Iterable, NoReturn, Sequence
+from collections.abc import Callable, Generator, Sequence
+from typing import TYPE_CHECKING, Any, NoReturn
 
 import numpy as np
-import pandas
+import pandas as pd
 import pytest
-from pandas import Series
 from pandas.tests.extension import base
+from typing_extensions import override
 
 from skfda.representation.basis import (
     Basis,
@@ -17,6 +19,9 @@ from skfda.representation.basis import (
     MonomialBasis,
 )
 
+if TYPE_CHECKING:
+    from pandas.api.extensions import ExtensionArray, ExtensionDtype
+
 
 ##############################################################################
 # Fixtures
@@ -26,37 +31,41 @@ from skfda.representation.basis import (
     FourierBasis(n_basis=5),
     BSplineBasis(n_basis=5),
 ])
-def basis(request: Any) -> Any:
+def basis(request: pytest.FixtureRequest) -> Basis:
     """A fixture providing the ExtensionDtype to validate."""
     return request.param
 
 
 @pytest.fixture
-def dtype(basis: Basis) -> FDataBasisDType:
+def dtype(basis: Basis) -> ExtensionDtype:
     """A fixture providing the ExtensionDtype to validate."""
     return FDataBasisDType(basis=basis)
 
 
 @pytest.fixture
-def data(basis: Basis) -> FDataBasis:
+def data(basis: Basis) -> ExtensionArray:
     """
-    Length-100 array for this type.
+    Return data.
+
+    Length-10 array for this type.
+
     * data[0] and data[1] should both be non missing
     * data[0] and data[1] should not be equal
+
     """
-    coef_matrix = np.arange(100 * basis.n_basis).reshape(100, basis.n_basis)
+    coef_matrix = np.arange(10 * basis.n_basis).reshape(10, basis.n_basis)
 
     return FDataBasis(basis=basis, coefficients=coef_matrix)
 
 
 @pytest.fixture
 def data_for_twos() -> NoReturn:
-    """Length-100 array in which all the elements are two."""
+    """Return a length-10 array in which all the elements are two."""
     raise NotImplementedError
 
 
 @pytest.fixture
-def data_missing(basis: Basis) -> FDataBasis:
+def data_missing(basis: Basis) -> ExtensionArray:
     """Length-2 array with [NA, Valid]."""
     coef_matrix = np.arange(
         2 * basis.n_basis,
@@ -68,41 +77,55 @@ def data_missing(basis: Basis) -> FDataBasis:
 
 
 @pytest.fixture(params=["data", "data_missing"])
-def all_data(request: Any, data: Any, data_missing: Any) -> Any:
-    """Parametrized fixture giving 'data' and 'data_missing'"""
+def all_data(
+    request: pytest.FixtureRequest,
+    data: ExtensionArray,
+    data_missing: ExtensionArray,
+) -> ExtensionArray:
+    """Return 'data' or 'data_missing'."""
     if request.param == "data":
         return data
-    elif request.param == "data_missing":
+
+    if request.param == "data_missing":
         return data_missing
+
+    msg = "Unreachable"
+    raise ValueError(msg)
 
 
 @pytest.fixture
-def data_repeated(data: FDataBasis) -> Callable[[int], Iterable[FDataBasis]]:
+def data_repeated(
+    data: ExtensionArray,
+) -> Callable[[int], Generator[ExtensionArray, None, None]]:
     """
     Generate many datasets.
-    Parameters
-    ----------
-    data : fixture implementing `data`
-    Returns
-    -------
-    Callable[[int], Generator]:
+
+    Args:
+        data : Fixture implementing `data`
+
+    Returns:
         A callable that takes a `count` argument and
         returns a generator yielding `count` datasets.
+
     """
 
-    def gen(count: int) -> Iterable[FDataBasis]:
-        for _ in range(count):
-            yield data
+    def gen(count: int) -> Generator[ExtensionArray, None, None]:
+        yield from (
+            data for _ in range(count)
+        )
 
     return gen
-
 
 @pytest.fixture
 def data_for_sorting() -> NoReturn:
     """
     Length-3 array with a known sort order.
+
     This should be three items [B, C, A] with
     A < B < C
+
+    For boolean dtypes (for which there are only 2 values available),
+    set B=C=True
     """
     raise NotImplementedError
 
@@ -111,6 +134,7 @@ def data_for_sorting() -> NoReturn:
 def data_missing_for_sorting() -> NoReturn:
     """
     Length-3 array with a known sort order.
+
     This should be three items [B, NA, A] with
     A < B and NA missing.
     """
@@ -118,32 +142,37 @@ def data_missing_for_sorting() -> NoReturn:
 
 
 @pytest.fixture
-def na_cmp() -> Callable[[Any, Any], bool]:
+def na_cmp() -> Callable[..., bool]:
     """
     Binary operator for comparing NA values.
+
     Should return a function of two arguments that returns
     True if both arguments are (scalar) NA for your type.
     By default, uses ``operator.is_``
     """
-    def isna(x: Any, y: Any) -> bool:
+    def isna(
+        x: pd.api.typing.NAType | FDataBasis,
+        y: pd.api.typing.NAType | FDataBasis,
+    ) -> bool:
         return (
-            (x is pandas.NA or all(x.isna()))
-            and (y is pandas.NA or all(y.isna()))
+            (x is pd.NA or all(x.isna()))
+            and (y is pd.NA or all(y.isna()))
         )
 
     return isna
 
 
 @pytest.fixture
-def na_value() -> pandas.NA:
-    """The scalar missing value for this type. Default 'None'"""
-    return pandas.NA
+def na_value() -> pd.api.typing.NAType:
+    """Return the scalar missing value for this type. Default 'None'."""
+    return pd.NA
 
 
 @pytest.fixture
 def data_for_grouping() -> NoReturn:
     """
-    Data for factorization, grouping, and unique tests.
+    Return data for factorization, grouping, and unique tests.
+
     Expected to be like [B, B, NA, NA, A, A, B, C]
     Where A < B < C and NA is missing
     """
@@ -151,46 +180,44 @@ def data_for_grouping() -> NoReturn:
 
 
 @pytest.fixture(params=[True, False])
-def box_in_series(request: Any) -> Any:
-    """Whether to box the data in a Series"""
+def box_in_series(request: pytest.FixtureRequest) -> bool:
+    """Whether to box the data in a Series."""
     return request.param
 
 
 @pytest.fixture(
-    params=[
-        lambda x: 1,
+    params=list[Callable[[FDataBasis], Any]]([
+        lambda x: 1,  # noqa: ARG005
         lambda x: [1] * len(x),
-        lambda x: Series([1] * len(x)),
+        lambda x: pd.Series([1] * len(x)),
         lambda x: x,
-    ],
+    ]),
     ids=["scalar", "list", "series", "object"],
 )
-def groupby_apply_op(request: Any) -> Any:
-    """
-    Functions to test groupby.apply().
-    """
+def groupby_apply_op(
+    request: pytest.FixtureRequest,
+) -> Callable[[FDataBasis], Any]:
+    """Functions to test groupby.apply()."""
     return request.param
 
 
 @pytest.fixture(params=[True, False])
-def as_frame(request: Any) -> Any:
-    """
-    Boolean fixture to support Series and Series.to_frame() comparison testing.
-    """
+def as_frame(request: pytest.FixtureRequest) -> bool:
+    """Whether to support Series and Series.to_frame() comparison testing."""
     return request.param
 
 
 @pytest.fixture(params=[True, False])
-def as_series(request: Any) -> Any:
-    """
-    Boolean fixture to support arr and Series(arr) comparison testing.
-    """
+def as_series(request: pytest.FixtureRequest) -> bool:
+    """Boolean fixture to support arr and Series(arr) comparison testing."""
     return request.param
 
 
 @pytest.fixture(params=[True, False])
-def use_numpy(request: Any) -> Any:
+def use_numpy(request: pytest.FixtureRequest) -> bool:
     """
+    Compare ExtensionDtype and numpy.
+
     Boolean fixture to support comparison testing of ExtensionDtype array
     and numpy array.
     """
@@ -198,8 +225,10 @@ def use_numpy(request: Any) -> Any:
 
 
 @pytest.fixture(params=["ffill", "bfill"])
-def fillna_method(request: Any) -> Any:
+def fillna_method(request: pytest.FixtureRequest) -> str:
     """
+    Series.fillna parameter fixture.
+
     Parametrized fixture giving method parameters 'ffill' and 'bfill' for
     Series.fillna(method=<method>) testing.
     """
@@ -207,10 +236,8 @@ def fillna_method(request: Any) -> Any:
 
 
 @pytest.fixture(params=[True, False])
-def as_array(request: Any) -> Any:
-    """
-    Boolean fixture to support ExtensionDtype _from_sequence method testing.
-    """
+def as_array(request: pytest.FixtureRequest) -> bool:
+    """Whether to support ExtensionDtype _from_sequence method testing."""
     return request.param
 
 
@@ -233,20 +260,16 @@ _all_arithmetic_operators = [
 
 
 @pytest.fixture(params=_all_arithmetic_operators)
-def all_arithmetic_operators(request: Any) -> Any:
-    """
-    Fixture for dunder names for common arithmetic operations.
-    """
+def all_arithmetic_operators(request: pytest.FixtureRequest) -> str:
+    """Fixture for dunder names for common arithmetic operations."""
     return request.param
 
 
 @pytest.fixture(params=["__eq__", "__ne__",
                         # "__le__", "__lt__", "__ge__", "__gt__"
                         ])
-def all_compare_operators(request: Any) -> Any:
-    """
-    Fixture for dunder names for common compare operations
-    """
+def all_compare_operators(request: pytest.FixtureRequest) -> str:
+    """Fixture for dunder names for common compare operations."""
     return request.param
 
 
@@ -265,10 +288,8 @@ _all_numeric_reductions = [
 
 
 @pytest.fixture(params=_all_numeric_reductions)
-def all_numeric_reductions(request: Any) -> Any:
-    """
-    Fixture for numeric reduction names.
-    """
+def all_numeric_reductions(request: pytest.FixtureRequest) -> str:
+    """Fixture for numeric reduction names."""
     return request.param
 
 
@@ -279,10 +300,8 @@ _all_boolean_reductions: Sequence[str] = [
 
 
 @pytest.fixture(params=_all_boolean_reductions)
-def all_boolean_reductions(request: Any) -> Any:
-    """
-    Fixture for boolean reduction names.
-    """
+def all_boolean_reductions(request: pytest.FixtureRequest) -> str:
+    """Fixture for boolean reduction names."""
     return request.param
 
 ##############################################################################
@@ -291,145 +310,228 @@ def all_boolean_reductions(request: Any) -> Any:
 
 
 class TestCasting(base.BaseCastingTests):  # type: ignore[misc]
+    """Casting to and from the ExtensionDtype."""
 
     # Tries to construct dtype from string
     @pytest.mark.skip(reason="Unsupported")
-    def test_astype_str(self) -> None:
+    @override
+    def test_astype_str(self, data: ExtensionArray) -> None:
         pass
 
     # Tries to construct dtype from string
     @pytest.mark.skip(reason="Unsupported")
-    def test_astype_string(self) -> None:
+    @override
+    def test_astype_string(
+        self,
+        data: ExtensionArray,
+        nullable_string_dtype: ExtensionDtype,
+    ) -> None:
         pass
 
 
 class TestConstructors(base.BaseConstructorsTests):  # type: ignore[misc]
+    """Tests of constructors."""
 
     # Does not support scalars which are also ExtensionArrays
     @pytest.mark.skip(reason="Unsupported")
-    def test_series_constructor_scalar_with_index(self) -> None:
+    @override
+    def test_series_constructor_scalar_with_index(
+        self,
+        data: ExtensionArray,
+        dtype: ExtensionDtype,
+    ) -> None:
         pass
 
     # Tries to construct dtype from string
     @pytest.mark.skip(reason="Unsupported")
-    def test_from_dtype(self) -> None:
+    @override
+    def test_from_dtype(
+        self,
+        data: ExtensionArray,
+    ) -> None:
         pass
-
 
 class TestDtype(base.BaseDtypeTests):  # type: ignore[misc]
+    """Tests of the ExtensionDtype."""
 
     # Tries to construct dtype from string
     @pytest.mark.skip(reason="Unsupported")
-    def test_construct_from_string_own_name(self) -> None:
+    @override
+    def test_construct_from_string_own_name(
+        self,
+        dtype: ExtensionDtype,
+    ) -> None:
         pass
 
     # Tries to construct dtype from string
     @pytest.mark.skip(reason="Unsupported")
-    def test_is_dtype_from_name(self) -> None:
+    @override
+    def test_is_dtype_from_name(
+        self,
+        dtype: ExtensionDtype,
+    ) -> None:
         pass
 
     # Tries to construct dtype from string
     @pytest.mark.skip(reason="Unsupported")
-    def test_eq_with_str(self) -> None:
-        pass
-
-    # Tries to construct dtype from string
-    @pytest.mark.skip(reason="Unsupported")
-    def test_construct_from_string(self, dtype: Any) -> None:
+    @override
+    def test_eq_with_str(
+        self,
+        dtype: ExtensionDtype,
+    ) -> None:
         pass
 
 
 class TestGetitem(base.BaseGetitemTests):  # type: ignore[misc]
-    pass
+    """Tests for ExtensionArray.__getitem__."""
 
+    # We do not support readonly property
+    @pytest.mark.skip(reason="Unsupported")
+    @override
+    def test_getitem_propagates_readonly_property(
+        self,
+        data: ExtensionArray,
+    ) -> None:
+        pass
 
 class TestInterface(base.BaseInterfaceTests):  # type: ignore[misc]
+    """Tests that the basic interface is satisfied."""
 
     # Does not support scalars which are also array_like
     @pytest.mark.skip(reason="Unsupported")
-    def test_array_interface(self) -> None:
+    @override
+    def test_array_interface(
+        self,
+        data: ExtensionArray,
+    ) -> None:
         pass
 
     # We do not implement setitem
     @pytest.mark.skip(reason="Unsupported")
-    def test_copy(self, dtype: Any) -> None:
+    @override
+    def test_copy(
+        self,
+        data: ExtensionArray,
+    ) -> None:
         pass
 
     # We do not implement setitem
     @pytest.mark.skip(reason="Unsupported")
-    def test_view(self, dtype: Any) -> None:
+    @override
+    def test_view(
+        self,
+        data: ExtensionArray,
+    ) -> None:
         pass
 
     # Pending https://github.com/pandas-dev/pandas/issues/38812 resolution
     @pytest.mark.skip(reason="Bugged")
-    def test_contains(self, data: Any, data_missing: Any) -> None:
+    @override
+    def test_contains(
+        self,
+        data: ExtensionArray,
+        data_missing: ExtensionArray,
+        using_nan_is_na: bool,
+    ) -> None:
         pass
 
     # We do not attempt to implement copy right now.
     @pytest.mark.skip(reason="Unsupported")
+    @override
     def test_array_interface_copy(
         self,
-        data: Any,
+        data: ExtensionArray,
     ) -> None:
         pass
 
 class TestArithmeticOps(base.BaseArithmeticOpsTests):  # type: ignore[misc]
+    """Various Series and DataFrame arithmetic ops methods."""
 
     series_scalar_exc = None
 
     # Bug introduced by https://github.com/pandas-dev/pandas/pull/37132
     @pytest.mark.skip(reason="Unsupported")
+    @override
     def test_arith_frame_with_scalar(
         self,
-        data: Any,
-        all_arithmetic_operators: Any,
+        data: ExtensionArray,
+        all_arithmetic_operators: Callable[..., Any],
     ) -> None:
         pass
 
     # FDatabasis does not implement division by non constant
     @pytest.mark.skip(reason="Unsupported")
-    def test_divmod_series_array(self, dtype: Any) -> None:
+    @override
+    def test_divmod_series_array(
+        self,
+        data: ExtensionArray,
+        data_for_twos: ExtensionArray,
+    ) -> None:
         pass
 
     # Does not convert properly a list of FData to a FData
     @pytest.mark.skip(reason="Unsupported")
-    def test_arith_series_with_array(self, dtype: Any) -> None:
-        pass
-
-    # Does not error on operations
-    @pytest.mark.skip(reason="Unsupported")
-    def test_error(self, dtype: Any) -> None:
+    @override
+    def test_arith_series_with_array(
+        self,
+        data: ExtensionArray,
+        all_arithmetic_operators: str,
+    ) -> None:
         pass
 
 
 class TestComparisonOps(base.BaseComparisonOpsTests):  # type: ignore[misc]
+    """Various Series and DataFrame comparison ops methods."""
 
     # Cannot be compared with 0
     @pytest.mark.skip(reason="Unsupported")
+    @override
     def test_compare_scalar(
         self,
-        data: Any,
-        all_compare_operators: Any,
+        data: ExtensionArray,
+        comparison_op: Callable[..., Any],
     ) -> None:
         pass
 
     # Not sure how to pass it. Should it be reimplemented?
     @pytest.mark.skip(reason="Unsupported")
+    @override
     def test_compare_array(
         self,
-        data: Any,
-        all_compare_operators: Any,
+        data: ExtensionArray,
+        comparison_op: Callable[..., Any],
     ) -> None:
         pass
 
 
-class TestNumericReduce(base.BaseNumericReduceTests):  # type: ignore[misc]
+class TestReduce(base.BaseReduceTests ):  # type: ignore[misc]
+    """Reduction specific tests."""
 
+    @override
+    def _supports_reduction(
+        self,
+        ser: pd.Series,
+        op_name: str,
+    ) -> bool:
+        return op_name in ["sum", "mean"]
+
+    # Unsupported for now
+    @pytest.mark.skip(reason="Unsupported")
+    @override
+    def test_reduce_frame(
+        self,
+        data: ExtensionArray,
+        all_numeric_reductions: str,
+        skipna: bool,
+    ) -> None:
+        pass
+
+    @override
     def check_reduce(
         self,
-        s: Any,
+        ser: pd.Series,
         op_name: str,
         skipna: bool,
     ) -> None:
-        result = getattr(s, op_name)(skipna=skipna)
+        result = getattr(ser, op_name)(skipna=skipna)
         assert result.n_samples == 1
